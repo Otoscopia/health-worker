7e812f95a102952d6bb310b1e72aabb531c0ac9d
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-10 15:19:29 +0800

    fix: Use Application instead of ContainerBox

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,7 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:otoscopia_health_worker/core/widgets/container_box.dart';
+import 'package:otoscopia_health_worker/features/app/presentation/pages/app.dart';
 import 'package:otoscopia_health_worker/features/authentication/presentation/pages/sign_in.dart';
 import 'package:otoscopia_health_worker/features/authentication/presentation/providers/authentication_provider.dart';
 
@@ -13,7 +13,7 @@ class Routes extends ConsumerWidget {
     var isAuthenticated = ref.watch(authenticationProvider).$uid;
 
     return isAuthenticated != null
-        ? ContainerBox(child: Container())
+        ? const Application()
         : const SignIn();
   }
 }
