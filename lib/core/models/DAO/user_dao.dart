6f229cd691132e433ce4c93f4ff43c260efd8d6f
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-10 15:07:34 +0800

    feat: Create a user dao that fetch, drop, and add user to the database

---
+++ lib/core/models/DAO/user_dao.dart
@@ -0,0 +1,15 @@
+import 'package:floor/floor.dart';
+
+import 'package:otoscopia_health_worker/features/authentication/data/models/user_model.dart';
+
+@dao
+abstract class UserDao {
+  @insert
+  Future<void> insertUser(UserModel user);
+
+  @Query('SELECT * FROM user')
+  Future<List<UserModel>> fetchUser();
+
+  @Query('DROP TABLE user')
+  Future<void> dropUser();
+}
