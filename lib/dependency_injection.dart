9adda3157f145821581688922c6d2ac6eae29b69
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-10 15:23:02 +0800

    feat: Initialize bitsdojo_window and window_manager

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,6 +1,9 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:window_manager/window_manager.dart';
 
-import 'config/models/env_model.dart';
+import 'core/constants/constants.dart';
+import 'core/models/env_model.dart';
 
 late Client client;
 late Account account;
@@ -22,5 +25,23 @@ class DependencyInjection {
         .setEndpoint(Env.appwriteEndPoint)
         .setProject(Env.appwriteProjectID)
         .setSelfSigned(status: true);
+
+    doWhenWindowReady(() {
+      final window = appWindow;
+      const initialWindowSize = applicationMinimumSize;
+      window.minSize = initialWindowSize;
+      window.size = initialWindowSize;
+      window.alignment = center;
+      window.title = applicationTitle;
+      window.show();
+    });
+
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      titleBarStyle: TitleBarStyle.hidden
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions);
   }
 }
