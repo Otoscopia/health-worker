39f2db2d507f4734c57a1a7eec5d143ac33aa9cd
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-11 04:18:19 +0800

    feat: Create filter cards

---
+++ lib/features/app/presentation/widgets/dashboard_widgets/filter_widgets/initial_diagnosis.dart
@@ -0,0 +1,21 @@
+import "package:fluent_ui/fluent_ui.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "package:health_worker/core/constants/constants.dart";
+
+import "filter_card.dart";
+
+class InitialDiagnosis extends ConsumerWidget {
+  const InitialDiagnosis({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const FilterCard(
+      icon: initialIcon,
+      title: initial,
+      amount: 0,
+    );
+  }
+}
