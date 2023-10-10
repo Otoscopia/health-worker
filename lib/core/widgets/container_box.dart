9684837c9212c0cb3a7a21915a05fdfc3d88bd27
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-10 13:41:50 +0800

    feat: Create ContainerBox that displays Column with custom TitleBar

---
+++ lib/core/widgets/container_box.dart
@@ -0,0 +1,15 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:otoscopia_health_worker/core/widgets/window_title_bar.dart';
+
+class ContainerBox extends ConsumerWidget {
+  final Widget child;
+  const ContainerBox({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      children: [const TitleBar(), Expanded(child: child)],
+    );
+  }
+}
