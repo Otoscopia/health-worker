8641e0c7c98f9f04a2d3c57671c0ef0b4667771a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-10 13:45:41 +0800

    feat: Create constants.dart to be used throught the application

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,32 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+const String applicationTitle = "Otoscopia - Health Worker";
+
+// application minimum width and height
+const applicationMinimumSize = Size(1200, 700);
+const center = Alignment.center;
+
+const String welcomeLabel = "Welcome Back!";
+const String passwordLabel = "Password";
+const String emailLabel = "Email Address";
+
+// Spacer
+const smallWidth = SizedBox(width: 4);
+const mediumWidth = SizedBox(width: 8);
+const largeWidth = SizedBox(width: 16);
+
+const smallHeight = SizedBox(height: 4);
+const mediumHeight = SizedBox(height: 8);
+const largeHeight = SizedBox(height: 16);
+
+const smallPadding = EdgeInsets.all(4);
+const mediumPadding = EdgeInsets.all(8);
+const largePadding = EdgeInsets.all(16);
+
+const errorSeverity = InfoBarSeverity.error;
+const successSeverity = InfoBarSeverity.success;
+const warningSeverity = InfoBarSeverity.warning;
+
+const String errorLabel = "Ohh oh!";
+const String errorMessage = "Something went wrong, please try again later";
+const String invalidCredentials = "Inavlid credentials, please check your email and or password";
