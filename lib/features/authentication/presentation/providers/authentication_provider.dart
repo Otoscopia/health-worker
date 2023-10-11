188841e2aaed54ab8fb6265a5801f25187c0f7f2
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-11 23:26:46 +0800

    refractor: Uses AuthenticationEntity and call applicationProvider to change application state

--- lib/features/authentication/presentation/providers/authentication_provider.dart
+++ lib/features/authentication/presentation/providers/authentication_provider.dart
@@ -1,75 +1,38 @@
-import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:health_worker/core/constants/constants.dart';
 
 import 'package:health_worker/dependency_injection.dart';
-import 'package:health_worker/features/authentication/data/models/user_model.dart';
-import 'package:health_worker/features/authentication/domain/entity/user_entity.dart';
+import 'package:health_worker/features/authentication/domain/entity/authentication_entity.dart';
+import 'package:health_worker/features/authentication/presentation/providers/application_provider.dart.dart';
 
-class AuthenticationNotifier extends StateNotifier<UserEntity> {
-  AuthenticationNotifier() : super(UserEntity());
-
-  setUser(User user) async {
-    List<UserModel> userData = await database.userDao.fetchUser();
-
-    UserModel current = UserModel(
-      id: userData.length,
-      uid: user.$id,
-      createdAt: user.$createdAt,
-      updatedAt: user.$createdAt,
-      name: user.name,
-      registration: user.registration,
-      passwordUpdate: user.passwordUpdate,
-      email: user.email,
-      phone: user.phone,
-      accessedAt: user.accessedAt,
-      emailVerification: user.emailVerification,
-      phoneVerification: user.phoneVerification,
-      prefs: user.prefs.toString(),
-      status: user.status,
-      labels: user.labels.toString(),
-    );
-
-    if (userData.isNotEmpty) {
-      state = current;
-    } else {
-      state = current;
-      database.userDao.insertUser(current);
-    }
-  }
-
-  setUserFromDb(UserEntity user) {
-    state = user;
-  }
+class AuthenticationNotifier extends StateNotifier<AuthenticationEntity> {
+  AuthenticationNotifier() : super(AuthenticationEntity(loading: false, error: false));
 
   signIn(String email, String password) async {
-    Session session =
-        await account.createEmailSession(email: email, password: password);
+    final container = ProviderContainer();
+    final provider = container.read(applicationProvider.notifier);
 
-    if (session.current) {
-      User user = await account.get();
-      setUser(user);
+    setProvider(true, false, "");
+
+    try {
+      Session session = await account.createEmailSession(email: email, password: password);
+      if (session.current) {
+        User user = await account.get();
+        provider.setUserFromCloud(user);
+        setProvider(true, false, "");
+      }
+    } catch (error) {
+      setProvider(false, true, error.toString());
     }
   }
 
-  signOut() async {
-    try {
-      account.deleteSession(sessionId: currenSession);
-    } on AppwriteException catch (error) {
-      if (error.message != null) {
-        if (error.message.toString().contains(offlineErrorMessage)) {
-          state = UserEntity();
-          database.userDao.dropUser();
-        }
-      }
-    }
-    state = UserEntity();
-    database.userDao.dropUser();
+  setProvider(bool loading, bool error, String? errorMessage) {
+    state = AuthenticationEntity(
+        loading: loading, error: error, errorMessage: errorMessage);
   }
 }
 
 final authenticationProvider =
-    StateNotifierProvider<AuthenticationNotifier, UserEntity>((ref) {
+    StateNotifierProvider<AuthenticationNotifier, AuthenticationEntity>((ref) {
   return AuthenticationNotifier();
 });
