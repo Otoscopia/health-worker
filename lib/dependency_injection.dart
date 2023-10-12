3d4e93a31cbd329e6d5c7ea2bfb09bac71a746da
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-13 04:02:47 +0800

    feat: Add Teams, TeamList and FlutterSecureStorage

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,14 +1,20 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'core/models/application_database.dart';
+
 import 'core/constants/constants.dart';
 import 'core/models/env_model.dart';
 
 late Client client;
 late Account account;
+late Teams team;
+late TeamList teamList;
 late ApplicationDatabase database;
+late FlutterSecureStorage storage;
 
 class DependencyInjection {
   static final DependencyInjection _singleton = DependencyInjection._internal();
@@ -21,12 +27,17 @@ class DependencyInjection {
 
   Future<void> initialize() async {
     client = Client();
-    account = Account(client);
 
     client
         .setEndpoint(Env.appwriteEndPoint)
         .setProject(Env.appwriteProjectID)
         .setSelfSigned(status: true);
+
+    account = Account(client);
+
+    team = Teams(client);
+    
+    storage = const FlutterSecureStorage();
 
     doWhenWindowReady(() {
       final window = appWindow;
