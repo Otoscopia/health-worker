321302f4a3e3505429234f00b35a33b1c1160d30
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-16 00:36:26 +0800

    feat: Move spacing to its own files

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -13,19 +13,6 @@ const center = Alignment.center;
 const String currenSession = "current";
 const String offlineErrorMessage = "not connected";
 
-// Spacer
-const smallWidth = SizedBox(width: 4);
-const mediumWidth = SizedBox(width: 8);
-const largeWidth = SizedBox(width: 16);
-
-const smallHeight = SizedBox(height: 4);
-const mediumHeight = SizedBox(height: 8);
-const largeHeight = SizedBox(height: 16);
-
-const smallPadding = EdgeInsets.all(4);
-const mediumPadding = EdgeInsets.all(8);
-const largePadding = EdgeInsets.all(16);
-
 // Error Constants
 const String errorLabel = "Ohh oh!";
 const errorSeverity = InfoBarSeverity.error;
