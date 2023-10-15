45b3f2367f2e5e301ce0ee4792b36f4ffb48ae48
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-15 16:19:33 +0800

    feat: Change email to username

---
+++ lib/features/authentication/presentation/widgets/username_text_box.dart
@@ -0,0 +1,22 @@
+import "package:fluent_ui/fluent_ui.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "package:health_worker/core/constants.dart";
+import "package:health_worker/features/authentication/domain/provider/email_provider.dart";
+
+class UsernameTextBox extends ConsumerWidget {
+  const UsernameTextBox({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InfoLabel(
+      label: usernameLabel,
+      child: TextFormBox(
+        placeholder: usernameLabel,
+        expands: false,
+        onChanged: (value) => ref.watch(usernameProvider.notifier).setUsername(value),
+        decoration: const BoxDecoration(border: Border.fromBorderSide(BorderSide.none)),
+      ),
+    );
+  }
+}
