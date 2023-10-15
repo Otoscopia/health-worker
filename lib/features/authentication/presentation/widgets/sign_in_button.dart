a31fd7eb4b59637a1c008e38e67f6acd1675a4f0
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-15 16:21:16 +0800

    refractor: Removed parameters and use riverpod state notifier

--- lib/features/authentication/presentation/widgets/sign_in_button.dart
+++ lib/features/authentication/presentation/widgets/sign_in_button.dart
@@ -1,32 +1,23 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:health_worker/core/constants/constants.dart';
-import 'package:health_worker/core/widgets/info_bar_pop_up.dart';
-import 'package:health_worker/features/authentication/presentation/providers/authentication_provider.dart';
+import 'package:health_worker/core/constants.dart';
+import 'package:health_worker/core/widgets.dart';
+import 'package:health_worker/features/authentication/domain/usecase/sign_in_usecase.dart';
+import 'package:health_worker/features/authentication/presentation/provider/authentication_provider.dart';
 
 class SignInButton extends ConsumerWidget {
-  final GlobalKey<FormState> globalKey;
-  final TextEditingController email, password;
-  const SignInButton(
-      {super.key,
-      required this.email,
-      required this.password,
-      required this.globalKey});
+  const SignInButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     bool loading = ref.watch(authenticationProvider).loading;
     bool error = ref.watch(authenticationProvider).error;
-    String? errorMessage = ref.watch(authenticationProvider).errorMessage;
+    String? errorMessage = ref.watch(authenticationProvider).errorMesage;
 
     if (error) {
       if (errorMessage != null) {
-        String errorCode = errorMessage.split(',')[0].split(':')[1].trim();
-        if (errorMessageObject.containsKey(errorCode)) {
-          String errorDescription = errorMessageObject[errorCode]!;
-          popUpInfoBar(context, InfoBarSeverity.error, errorLabel, errorDescription, 5);
-        }
+          popUpInfoBar(context, InfoBarSeverity.error, errorLabel, errorMessage, 5);
       }
     }
 
@@ -36,15 +27,8 @@ class SignInButton extends ConsumerWidget {
             width: 150,
             child: FilledButton(
               child: const Text(signInButton),
-              onPressed: () =>
-                  signIn(ref, email.text, password.text, globalKey),
+              onPressed: () => signIn(context, ref),
             ),
           );
-  }
-}
-
-signIn(WidgetRef ref, String email, String password, GlobalKey<FormState> key) {
-  if (key.currentState!.validate() && password.length >= 8) {
-    ref.watch(authenticationProvider.notifier).signIn(email, password);
   }
 }
