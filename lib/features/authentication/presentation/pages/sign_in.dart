28fbc03b1fbcfab7961323ce5cbad0393b199559
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-15 16:23:42 +0800

    refractor: Made widget to provider ConsumerWidget

--- lib/features/authentication/presentation/pages/sign_in.dart
+++ lib/features/authentication/presentation/pages/sign_in.dart
@@ -3,27 +3,15 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:styled_widget/styled_widget.dart";
 
 import "package:health_worker/config/themes/colors.dart";
-import "package:health_worker/core/constants/constants.dart";
-import "package:health_worker/features/authentication/presentation/widgets/email_text_box.dart";
-import "package:health_worker/features/authentication/presentation/widgets/logo_container.dart";
-import "package:health_worker/features/authentication/presentation/widgets/password_text_box.dart";
-import "package:health_worker/features/authentication/presentation/widgets/sign_in_button.dart";
-import 'package:health_worker/core/widgets/container_box.dart';
+import "package:health_worker/core/constants.dart";
+import 'package:health_worker/core/widgets.dart';
+import 'package:health_worker/features/authentication/presentation/widgets.dart';
 
-class SignIn extends ConsumerStatefulWidget {
+class SignIn extends ConsumerWidget {
   const SignIn({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _SignInState();
-}
-
-class _SignInState extends ConsumerState<SignIn> {
-  final key = GlobalKey<FormState>();
-  TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ScaffoldPage(
       padding: EdgeInsets.zero,
       content: ContainerBox(
@@ -42,11 +30,11 @@ class _SignInState extends ConsumerState<SignIn> {
                     const LogoContainer(),
                     const Text(welcomeLabel).bold().fontSize(18),
                     largeHeight,
-                    EmailTextBox(email: email),
+                    const UsernameTextBox(),
                     largeHeight,
-                    PasswordTextBox(password: password),
+                    const PasswordTextBox(),
                     largeHeight,
-                    SignInButton(email: email, password: password, globalKey: key),
+                    const SignInButton(),
                   ],
                 ),
               ),
