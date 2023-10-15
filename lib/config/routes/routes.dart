1e21d98e4a28d9ab11886b09b706b83441e82859
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-15 20:12:30 +0800

    feat: Add offline ability

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,29 +1,44 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:health_worker/config/routes/future_functions.dart';
+import 'package:health_worker/core/provider/application_provider.dart';
 import 'package:health_worker/core/widgets.dart';
+import 'package:health_worker/core/widgets/offline_bar.dart';
 import 'package:health_worker/features/app/presentation/pages/dashboard.dart';
 import 'package:health_worker/features/authentication/presentation/pages/sign_in.dart';
+import 'package:health_worker/features/authentication/presentation/provider/authentication_provider.dart';
 
 class Routes extends ConsumerWidget {
   const Routes({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    bool? authenticated = false;
-
-    return FutureBuilder(
-      future: loadUser(ref, authenticated),
-      builder: ((context, snapshot) {
-        if (snapshot.data == false) {
-          return const SignIn();
-        } else if (snapshot.data == true) {
-          return const Dashboard();
-        } else {
-          return const LoadingAnimation();
-        }
-      }),
+    bool authenticated = ref.watch(authenticationProvider).authenticated;
+    return OfflineBuilder(
+      connectivityBuilder: (BuildContext context, ConnectivityResult connectivity, Widget child) {
+        final bool connected = connectivity != ConnectivityResult.none;
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          ref.read(applicationStateProvider.notifier).setConnected(connected);
+        });
+        return Stack(
+          fit: StackFit.expand,
+          children: [child, OfflineBar(connected: connected)],
+        );
+      },
+      child: FutureBuilder(
+        future: loadUser(ref, authenticated, ref.watch(applicationStateProvider).connected),
+        builder: ((context, snapshot) {
+          if (snapshot.data == false) {
+            return const SignIn();
+          } else if (snapshot.data == true) {
+            return const Dashboard();
+          } else {
+            return const LoadingAnimation();
+          }
+        }),
+      ),
     );
   }
 }
