d50c092dbc2ddffe13b8195ce5b685267d13c003
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-15 16:07:51 +0800

    chore (deps): Remove appwrite v11.0.0, floor and floor_generator v1.4.2

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,9 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_windows
   screen_retriever
   system_theme
-  url_launcher_windows
   window_manager
-  window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
