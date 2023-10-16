8d66510533db36d5050e605cf7ca8ea6502a3931
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 03:33:40 +0800

    fix: Add  filePath and cheifComplain

--- lib/features/app/domain/entity/screening_entity.dart
+++ lib/features/app/domain/entity/screening_entity.dart
@@ -1,22 +1,32 @@
 class ScreeningEntity {
-  String historyOfIllness, healthWorkerComment, frameOfInterest, complains;
-  double temperature, bloodPressure, height, weight;
-  bool hasSimilarCondition, hasAllergies, patientUndergoSurgery, patientTakingMedication;
+  String historyOfIllness;
+  String healthWorkerComment;
+  String frameOfInterest;
+  String filePath;
+  String bloodPressure;
+  double temperature, height, weight;
+  bool hasSimilarCondition;
+  bool hasAllergies;
+  bool patientUndergoSurgery;
+  bool patientTakingMedication;
   String? chiefComplainMessage, patientTakingMedicationMessage;
+  List<String> cheifComplain;
 
-  ScreeningEntity(
-      {required this.historyOfIllness,
-      required this.healthWorkerComment,
-      required this.frameOfInterest,
-      required this.complains,
-      required this.temperature,
-      required this.bloodPressure,
-      required this.height,
-      required this.weight,
-      required this.hasSimilarCondition,
-      required this.hasAllergies,
-      required this.patientUndergoSurgery,
-      required this.patientTakingMedication,
-      this.chiefComplainMessage,
-      this.patientTakingMedicationMessage});
+  ScreeningEntity({
+    required this.historyOfIllness,
+    required this.healthWorkerComment,
+    required this.frameOfInterest,
+    required this.temperature,
+    required this.bloodPressure,
+    required this.height,
+    required this.weight,
+    required this.hasSimilarCondition,
+    required this.cheifComplain,
+    required this.patientUndergoSurgery,
+    required this.hasAllergies,
+    required this.patientTakingMedication,
+    required this.chiefComplainMessage,
+    required this.patientTakingMedicationMessage,
+    required this.filePath,
+  });
 }
