1eada7c538057a4c3dc093a07a7d3d41f08db49c
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 03:02:43 +0800

    feat: create screening information constants

---
+++ lib/core/constants/screening_information_constants.dart
@@ -0,0 +1,9 @@
+String diagnosisTitle =  "Diagnosis Information Form";
+
+const List<String> complains = [
+    "Ear Pain",
+    "Hearing Loss",
+    "Tinnittus",
+    "Ear Discharge",
+    "Others"
+  ];
