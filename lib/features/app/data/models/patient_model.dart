5937323b36ae1c346115259a4a6c6f4ef4e1ec43
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 03:30:42 +0800

    feat: Add createAt and avatar table

--- lib/features/app/data/models/patient_model.dart
+++ lib/features/app/data/models/patient_model.dart
@@ -1,7 +1,7 @@
-import 'package:health_worker/features/authentication/data/models/user_model.dart';
 import 'package:isar/isar.dart';
 
-import 'package:health_worker/features/app/domain/entity/patient_entity.dart';
+import 'package:health_worker/features/authentication/exports.dart';
+import 'package:health_worker/features/app/exports.dart';
 
 part 'patient_model.g.dart';
 
@@ -10,14 +10,18 @@ part 'patient_model.g.dart';
 class PatientModel extends PatientEntity {
   Id id = Isar.autoIncrement;
   final IsarLink<UserModel> createdBy = IsarLink<UserModel>();
+  DateTime createdAt;
+  String avatar;
 
   PatientModel({
     required super.uid,
+    required this.avatar,
     required super.fullName,
     required super.gender,
     required super.birthdate,
     required super.contactNumber,
     required super.schoolName,
     required super.schoolID,
+    required this.createdAt
   });
 }
