1d6a99478d0aab2408a24d651ba56aa76975feb3
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 03:02:32 +0800

    feat: Create camera constants

---
+++ lib/core/constants/camera_constants.dart
@@ -0,0 +1,14 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+const String noFilesFound = "You cannot continue unless you take a picture or a video";
+const String recheckCamera = "Recheck available cameras";
+const String disposeCamera = "Dipose Camera";
+
+const String createCamera = "Create Camera";
+const String cameraEmpty = "No available cameras";
+const String foundCamera = "Found camera:";
+
+const String failedCamera = "Failed to get cameras:";
+
+Widget rightEar = const Text("Right Ear").fontSize(24).bold();
