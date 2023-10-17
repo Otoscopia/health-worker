8044f0fc30b0f6f3f959b093620051173c073c80
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 12:50:14 +0800

    chore: Move some imports to add patient constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:ionicons/ionicons.dart';
-import 'package:styled_widget/styled_widget.dart';
 
 const String applicationTitle = "Otoscopia - Health Worker";
 const String applicationLogo = "assets/logo.svg";
@@ -10,7 +9,7 @@ const String applicationTitleBarLogo = "assets/icon.svg";
 const applicationMinimumSize = Size(1200, 700);
 const center = Alignment.center;
 
-const String currenSession = "current";
+const String currentSession = "current";
 const String offlineErrorMessage = "not connected";
 
 // Error Constants
@@ -53,41 +52,26 @@ const String genderLabel = "Gender";
 const String medicalRecordStatus = "Medical Record Status";
 const String uploadedDate = "Uploaded Date";
 
-const String addPatientLabel = "Add Patient";
-const String addPatientCancelTitle = "Cancel Adding new Patient?";
-const String addPatientCancelContent = "If you click proceed, your data will be lost";
-
 const String proceedLabel = "Proceed";
 const String continueLabel = "Continue";
-const String birthdayLabel = "Birthdate";
-const String genderTitle = "Gender*";
-const String maleLabel = "Male";
-const String femaleLabel = "Female";
-Text invalidGender = const Text("Please input a valid gender").fontWeight(FontWeight.w100).textColor(Colors.red.lighter);
-Text invalidBirthdate = const Text("Please select a valid birthdate").fontWeight(FontWeight.w100).textColor(Colors.red.lighter);
 
 const Border borderNone = Border.fromBorderSide(BorderSide.none);
 
-
-const String schoolNameLabel = "School Name*";
 const String schoolNamePlaceholder = "Iligan City Division Office";
 const String schoolNameError = "Please input a valid school name";
 
-const String schoolIDLabel = "School ID*";
 const String schoolIDPlaceholder = "School ID";
 const String schoolIDError = "Please input a valid school id";
 
-const String fullNameLabel = "Patient Full Name*";
 const String fullNamePlaceholder = "Full Name";
 const String fullNameError = "Please input a valid name";
 
-const String contactNumberLabel = "Patient Contact Number*";
 const String contactNumberPlaceholder = "09XXXXXXXXX";
 const String contactNumberErrorOne = "Please input a valid contact number";
 const String contactNumberErrorTwo = "Contact number must start with 09";
 
 String dateOfExamination = "Date of Examination";
-String examiner = "Examiner";
+String examinerLabel = "Examiner";
 
 String historyOfIllness = "History of Illness";
 String healthWorkerComment = "Health worker comment";
