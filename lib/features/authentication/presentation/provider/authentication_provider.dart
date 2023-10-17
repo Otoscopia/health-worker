ebd4af4f7df8cfe5138c9e4281fa46d445f4d40c
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 12:40:20 +0800

    feat: Create connection to appwrite back-end

--- lib/features/authentication/presentation/provider/authentication_provider.dart
+++ lib/features/authentication/presentation/provider/authentication_provider.dart
@@ -1,10 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:health_worker/dependency_injection.dart';
-import 'package:health_worker/features/authentication/data/models/user_model.dart';
-
-import 'package:health_worker/features/authentication/presentation/provider/user_provider.dart';
-import 'package:health_worker/features/authentication/domain/entity/authentication_entity.dart';
 import 'package:isar/isar.dart';
+
+import 'package:health_worker/core/exports.dart';
+import 'package:health_worker/dependency_injection.dart';
+import 'package:health_worker/features/authentication/exports.dart';
 
 class AuthenticationNotifier extends StateNotifier<AuthenticationEntity> {
   final StateNotifierProviderRef<AuthenticationNotifier, AuthenticationEntity> ref;
@@ -13,25 +12,45 @@ class AuthenticationNotifier extends StateNotifier<AuthenticationEntity> {
   signIn(String email, String password) async {
     final provider = ref.read(userProvider.notifier);
     state = AuthenticationEntity(loading: true, error: false, authenticated: false);
+    final bool connected = ref.read(applicationStateProvider).connected;
 
     try {
-    state = AuthenticationEntity(loading: false, error: false, authenticated: true);
-    provider.setUser("Laurence Troy Sambaan Valdez", email, "09262309684");
-    } catch (error) {
-      state = AuthenticationEntity(loading: false, error: true, authenticated: false, errorMesage: "$error");
-    }
+      if (connected) {
+        session = await account.createEmailSession(email: email, password: password);
 
+        if (session.current) {
+          examiner = await account.get();
+
+          state = AuthenticationEntity(loading: false, error: false, authenticated: true);
+          provider.setUser(examiner.$id, examiner.name, examiner.email, examiner.labels);
+        }
+      } else {
+        state = AuthenticationEntity(loading: false, error: false, authenticated: true);
+        state = AuthenticationEntity(loading: false, error: true, authenticated: false, errorMesage: connectionError);
+      }
+    } catch (error) {
+      if (error.toString().isNotEmpty) {
+        String errorCode = error.toString().split(',')[0].split(':')[1].trim();
+        if (errorMessageObject.containsKey(errorCode)) {
+          String errorMessage = errorMessageObject[errorCode]!;
+          state = AuthenticationEntity(loading: false, error: true, authenticated: false, errorMesage: errorMessage);
+        }
+      }
+    }
   }
 
   signOut() async {
     final provider = ref.read(userProvider);
+    account.deleteSession(sessionId: currentSession);
     await isar.writeTxn(() async {
       await isar.userModels.filter().uidEqualTo(provider.uid).deleteAll();
     });
-    state = AuthenticationEntity(loading: false, error: false, authenticated: false);
+    state = AuthenticationEntity(
+        loading: false, error: false, authenticated: false);
   }
 }
 
-final authenticationProvider = StateNotifierProvider<AuthenticationNotifier, AuthenticationEntity>((ref) {
+final authenticationProvider =
+    StateNotifierProvider<AuthenticationNotifier, AuthenticationEntity>((ref) {
   return AuthenticationNotifier(ref);
 });
