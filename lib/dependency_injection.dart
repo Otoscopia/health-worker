92b52266f53b70aaacd5b542d2f998aa3774926c
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 12:36:49 +0800

    feat: Initialize appwrite and made late appwrite variables

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,16 +1,23 @@
 import 'dart:io';
 
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:window_manager/window_manager.dart';
 
+import 'package:health_worker/core/exports.dart';
 import 'package:health_worker/features/app/exports.dart';
 import 'package:health_worker/features/authentication/exports.dart';
 
-import 'core/constants/constants.dart';
-
+late Client client;
+late Account account;
+late Teams team;
+late TeamList teamList;
+late Session session;
+late User examiner;
 late FlutterSecureStorage storage;
 late Directory appDir;
 late String applicationPath;
@@ -26,6 +33,17 @@ class DependencyInjection {
   DependencyInjection._internal();
 
   Future<void> initialize() async {
+    client = Client();
+
+    client
+        .setEndpoint(Env.appwriteEndPoint)
+        .setProject(Env.appwriteProjectID)
+        .setSelfSigned(status: true);
+
+    account = Account(client);
+
+    team = Teams(client);
+    
     storage = const FlutterSecureStorage();
 
     appDir = await getApplicationSupportDirectory();
