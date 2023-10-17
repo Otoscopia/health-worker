097579e575c09b87a369fd0b8aeced71dbfe45c1
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-17 12:43:53 +0800

    refractor: Removed offline builder

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,5 +1,4 @@
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'future_functions.dart';
@@ -14,29 +13,20 @@ class Routes extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     bool authenticated = ref.watch(authenticationProvider).authenticated;
     bool connected = ref.watch(applicationStateProvider).connected;
-    return OfflineBuilder(
-      connectivityBuilder: (BuildContext context, ConnectivityResult connectivity, Widget child) {
-        final bool connected = connectivity != ConnectivityResult.none;
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          ref.read(applicationStateProvider.notifier).setConnected(connected);
-        });
-        return Stack(
-          fit: StackFit.expand,
-          children: [child, OfflineBar(connected: connected)],
-        );
-      },
-      child: FutureBuilder(
-        future: loadUser(ref, authenticated, connected),
-        builder: ((context, snapshot) {
-          if (snapshot.data == false) {
-            return const SignIn();
-          } else if (snapshot.data == true) {
-            return const Dashboard();
-          } else {
-            return const LoadingAnimation();
-          }
-        }),
-      ),
+
+    Future<bool> loadUserFuture = connected ? loadUser(ref, authenticated) : loadUserOffline(ref);
+
+    return FutureBuilder(
+      future: loadUserFuture,
+      builder: ((context, snapshot) {
+        if (snapshot.data == false) {
+          return const SignIn();
+        } else if (snapshot.data == true) {
+          return const Dashboard();
+        } else {
+          return const LoadingAnimation();
+        }
+      }),
     );
   }
 }
