d3bd85789c92d9ac650fb0df0c5e71a2264df69b
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-19 08:11:54 +0800

    feat: Add OfflineBuilder to tell the user when its offline

--- lib/core/widgets/container_box.dart
+++ lib/core/widgets/container_box.dart
@@ -1,7 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:health_worker/core/widgets/window_title_bar.dart';
+import 'package:health_worker/core/exports.dart';
 
 class ContainerBox extends ConsumerWidget {
   final Widget child;
@@ -9,8 +9,21 @@ class ContainerBox extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      children: [const TitleBar(), Expanded(child: child)],
+    return OfflineBuilder(
+      connectivityBuilder: (context, value, child) {
+        final bool connected = value != ConnectivityResult.none;
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          ref.read(applicationStateProvider.notifier).setConnected(connected);
+        });
+
+        return Stack(
+          fit: StackFit.expand,
+          children: [child, OfflineBar(connected: connected)],
+        );
+      },
+      child: Column(
+        children: [const TitleBar(), Expanded(child: child)],
+      ),
     );
   }
 }
