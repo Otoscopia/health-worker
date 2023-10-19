06af0169e5f72b7117097631385a3da58a215d28
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-19 08:11:33 +0800

    feat: Fetch user data from cloud when online and fetch records from db when offline

--- lib/config/routes/future_functions.dart
+++ lib/config/routes/future_functions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:health_worker/core/exports.dart';
+import 'package:health_worker/features/app/presentation/providers/fetch_patient_provider.dart';
+import 'package:health_worker/features/app/presentation/providers/fetch_screening_records_provider.dart';
 import 'package:isar/isar.dart';
 
 import 'package:health_worker/dependency_injection.dart';
@@ -11,9 +13,15 @@ Future<bool> loadUser(WidgetRef ref, bool isAuthenticated) async {
 
   try {
     session = await account.getSession(sessionId: currentSession);
+    examiner = await account.get();
     if (index < 1) {
       ref.watch(userProvider.notifier).setUser(examiner.$id, examiner.name, examiner.email, examiner.labels);
+      ref.watch(authenticationProvider.notifier).setAuthenicated(true);
     }
+    final fetchUser = await userCollection.where().anyId().findFirst();
+    ref.watch(userProvider.notifier).fetchUserFromDb(fetchUser!);
+    ref.watch(patientListProvider.notifier).fetchPatients();
+    ref.watch(screeningListProvider.notifier).fetchScreeningRecords();
     return true;
   } catch (error) {
     return false;
@@ -27,6 +35,8 @@ Future<bool> loadUserOffline(WidgetRef ref) async {
   if (index > 0) {
     final fetchUser = await userCollection.where().anyId().findFirst();
     ref.watch(userProvider.notifier).fetchUserFromDb(fetchUser!);
+    ref.watch(patientListProvider.notifier).fetchPatients();
+    ref.watch(screeningListProvider.notifier).fetchScreeningRecords();
     return true;
   }
   return false;
