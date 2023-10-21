9c8219c8cc97d073a49806a015c10ff7aa108016
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 02:59:25 +0800

    feat: Add patientList and screeningList parameter and wrap SingleChildScrollView to scroll configuration

--- lib/features/app/presentation/widgets/dashboard_widgets/table_widget.dart
+++ lib/features/app/presentation/widgets/dashboard_widgets/table_widget.dart
@@ -2,11 +2,15 @@ import "package:fluent_ui/fluent_ui.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "package:health_worker/config/themes/colors.dart";
+import "package:health_worker/features/app/data/models/patient_model.dart";
+import "package:health_worker/features/app/data/models/screening_model.dart";
 
 import "table_widgets/material_widget.dart";
 
 class TableWidget extends ConsumerWidget {
-  const TableWidget({super.key});
+  final List<PatientModel> patientList;
+  final List<ScreeningModel> screeningList;
+  const TableWidget({super.key, required this.patientList, required this.screeningList});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -17,9 +21,12 @@ class TableWidget extends ConsumerWidget {
         width: double.infinity,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(8),
-          child: const SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: MaterialWidget(),
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: MaterialWidget(patientList: patientList, screeningList: screeningList),
+            ),
           ),
         ),
       ),
