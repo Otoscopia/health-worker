9d07c3235a2f3793ee8cd1344c5ea97ffb019055
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 02:53:34 +0800

    feat: Add post medical records to cloud db

--- lib/features/app/domain/usestate/post_medical_record.dart
+++ lib/features/app/domain/usestate/post_medical_record.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
+import 'package:appwrite/appwrite.dart';
 import 'package:encrypt/encrypt.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:health_worker/core/exports.dart';
 import 'package:health_worker/dependency_injection.dart';
+import 'package:health_worker/features/app/domain/provider/refresh_table_provider.dart';
 import 'package:health_worker/features/app/exports.dart';
-import 'package:health_worker/features/app/presentation/providers/fetch_patient_provider.dart';
-import 'package:health_worker/features/app/presentation/providers/fetch_screening_records_provider.dart';
 import 'package:health_worker/features/authentication/exports.dart';
 import 'package:isar/isar.dart';
 
@@ -17,96 +19,53 @@ Future<void> uploadMedicalRecord(WidgetRef ref) async {
   final encrypter = Encrypter(AES(Key.fromUtf8(key)));
 
   final patientUID = ref.read(patientProvider).uid;
-  final patient = ref.read(patientProvider).fullName;
-  final gender = ref.read(patientProvider).gender;
-  final birthday = DateTime.parse(ref.read(patientProvider).birthdate);
-  final contactNumber = ref.read(patientProvider).contactNumber;
-  final schoolName = ref.read(patientProvider).schoolName;
-  final schoolId = ref.read(patientProvider).schoolID;
+  final screeningUID = ref.read(screeningProvider).uid;
 
-  PatientModel patientModel = PatientModel(
-    uid: patientUID,
-    fullName: EncryptionClass().encryptData(patient, encrypter),
-    gender: EncryptionClass().encryptData(gender, encrypter),
-    birthdate: EncryptionClass().encryptData(birthday.toString(), encrypter),
-    contactNumber: EncryptionClass().encryptData(contactNumber, encrypter),
-    schoolName: EncryptionClass().encryptData(schoolName, encrypter),
-    schoolID: EncryptionClass().encryptData(schoolId, encrypter),
-    avatar: '',
-    createdBy: user.uid,
-  )..createdAt = DateTime.now();
+  final path = "${appDir.parent.path}\\$patientUID";
 
-  final historyOfIllness = ref.read(screeningProvider).historyOfIllness;
-  final healthWorkerComment = ref.read(screeningProvider).healthWorkerComment;
-  final frameOfInterest = ref.read(screeningProvider).frameOfInterest;
-  final temperatureValue = ref.read(screeningProvider).temperature;
-  final bloodPressure = ref.read(screeningProvider).bloodPressure;
-  final heightValue = ref.read(screeningProvider).height;
-  final weightValue = ref.read(screeningProvider).weight;
-  final hasSimilarConditionValue =
-      ref.read(screeningProvider).hasSimilarCondition;
-  final cheifComplains = ref.read(screeningProvider).cheifComplain;
-  final hasAllergiesValue = ref.read(screeningProvider).patientUndergoSurgery;
-  final patientUndergoSurgeryValue = ref.read(screeningProvider).hasAllergies;
-  final patientTakingMedicationValue =
-      ref.read(screeningProvider).patientTakingMedication;
-  final directory = ref.read(screeningProvider).filePath;
-  final otherComplains = ref.read(screeningProvider).chiefComplainMessage;
-  final medicationComment =
-      ref.read(screeningProvider).patientTakingMedicationMessage;
+  ScreeningModel screened = encryptScreeningDetail(ref, encrypter, patientUID, user.uid);
+  PatientModel patient = await encryptedPatient(ref, encrypter, patientUID, user.uid, path);
 
-  ScreeningModel model = ScreeningModel(
-      uid: uuid.v4().toString(),
-      historyOfIllness:
-          EncryptionClass().encryptData(historyOfIllness, encrypter),
-      healthWorkerComment:
-          EncryptionClass().encryptData(healthWorkerComment, encrypter),
-      frameOfInterest:
-          EncryptionClass().encryptData(frameOfInterest, encrypter),
-      temperature: EncryptionClass().encryptData(temperatureValue, encrypter),
-      bloodPressure: EncryptionClass().encryptData(bloodPressure, encrypter),
-      height: EncryptionClass().encryptData(heightValue, encrypter),
-      weight: EncryptionClass().encryptData(weightValue, encrypter),
-      hasSimilarCondition: EncryptionClass()
-          .encryptData(hasSimilarConditionValue.toString(), encrypter),
-      cheifComplain:
-          EncryptionClass().encryptData(cheifComplains.toString(), encrypter),
-      patientUndergoSurgery:
-          EncryptionClass().encryptData(hasAllergiesValue, encrypter),
-      hasAllergies:
-          EncryptionClass().encryptData(patientUndergoSurgeryValue, encrypter),
-      patientTakingMedication: EncryptionClass()
-          .encryptData(patientTakingMedicationValue, encrypter),
-      filePath: EncryptionClass().encryptData(directory, encrypter),
-      chiefComplainMessage: otherComplains != null && otherComplains.isNotEmpty
-          ? EncryptionClass().encryptData(otherComplains, encrypter)
-          : medicationComment,
-      patientTakingMedicationMessage:
-          medicationComment != null && medicationComment.isNotEmpty
-              ? EncryptionClass().encryptData(medicationComment, encrypter)
-              : medicationComment,
-      status: 'Pending',
-      owner: EncryptionClass().encryptData(patientUID, encrypter),
-      createdBy: EncryptionClass().encryptData(user.uid, encrypter))
-    ..uploadedAt;
+  final Directory dir = Directory(path);
+  List<FileSystemEntity> leftFiles = await dir.list().where((event) => event.path.contains("left-") && event.path.endsWith("jpeg")).toList();
+  List<FileSystemEntity> rightFiles = await dir.list().where((event) => event.path.contains("right-") && event.path.endsWith("jpeg")).toList();
 
-  // final Directory dir = Directory(directory);
-  // dir.listSync().map((element) {
+  await Future.wait(leftFiles.map((file) async {
+    await encrypt(file, screeningUID, encrypter, path, patientUID);
+  }));
 
-  //   EncryptionClass().encryptFiles(File(element.path), encrypter, element., savePath)
-  //   element.path.contains('left-');
-  // });
+  await Future.wait(rightFiles.map((file) async {
+    await encrypt(file, screeningUID, encrypter, path, patientUID);
+  }));
 
   await isar.writeTxn(() async {
-    await isar.patientModels.put(patientModel);
-    await isar.screeningModels.put(model);
+    await isar.patientModels.put(patient);
+    await isar.screeningModels.put(screened);
   });
 
-  await ref.watch(patientListProvider.notifier).fetchPatients();
-  await ref.watch(screeningListProvider.notifier).fetchScreeningRecords();
-
-  if (ref.read(applicationStateProvider).connected) {}
   await setStatetoEmpty(ref);
+
+  bool value = ref.read(refreshProvider);
+  ref.read(refreshProvider.notifier).setState(!value);
+
+  if (ref.read(applicationStateProvider).connected) {
+    uplloadToDB(ref, patient, screened);
+  }
+}
+
+var id = [];
+
+Future<void> encrypt(FileSystemEntity file, String screeningUID, Encrypter encrypter, String path, String patientUID) async {
+  
+  String fileName = file.path.split("\\").last;
+  String prefix = fileName.split("-").first;
+  String capturedDate = fileName.split("_").sublist(1).join("-");
+  String name = "$prefix-$screeningUID-$capturedDate";
+  await EncryptionClass().encryptFiles(File(file.path), encrypter, name, path);
+  String encryptedFile = "${file.parent.path}\\${name.split(".").first}.aes";
+  var unique = ID.unique();
+  await cloud.createFile(bucketId: Env.appwriteScreeningStorage, fileId: unique, file: InputFile.fromPath(path: encryptedFile));
+  id.add(unique);
 }
 
 Future<void> setStatetoEmpty(WidgetRef ref) async {
@@ -130,4 +89,115 @@ Future<void> setStatetoEmpty(WidgetRef ref) async {
   await ref.read(undergoSurgeryProvider.notifier).setState(3);
   await ref.read(takingMedicationProvider.notifier).setState(3);
   await ref.read(medicationProvider.notifier).setState("");
+}
+
+ScreeningModel encryptScreeningDetail(WidgetRef ref, Encrypter encrypter, String patientUID, String createdBy) {
+  final historyOfIllness = ref.read(screeningProvider).historyOfIllness;
+  final healthWorkerComment = ref.read(screeningProvider).healthWorkerComment;
+  final frameOfInterest = ref.read(screeningProvider).frameOfInterest;
+  final temperatureValue = ref.read(screeningProvider).temperature;
+  final bloodPressure = ref.read(screeningProvider).bloodPressure;
+  final heightValue = ref.read(screeningProvider).height;
+  final weightValue = ref.read(screeningProvider).weight;
+  final hasSimilarConditionValue = ref.read(screeningProvider).hasSimilarCondition;
+  final cheifComplains = ref.read(screeningProvider).cheifComplain;
+  final hasAllergiesValue = ref.read(screeningProvider).patientUndergoSurgery;
+  final patientUndergoSurgeryValue = ref.read(screeningProvider).hasAllergies;
+  final patientTakingMedicationValue = ref.read(screeningProvider).patientTakingMedication;
+  final otherComplains = ref.read(screeningProvider).chiefComplainMessage;
+  final medicationComment = ref.read(screeningProvider).patientTakingMedicationMessage;
+
+  ScreeningModel model = ScreeningModel(
+    screenedAt: DateTime.now(),
+    uid: uuid.v4().toString(),
+    historyOfIllness: EncryptionClass().encryptData(historyOfIllness, encrypter),
+    healthWorkerComment: EncryptionClass().encryptData(healthWorkerComment, encrypter),
+    frameOfInterest: EncryptionClass().encryptData(frameOfInterest, encrypter),
+    temperature: EncryptionClass().encryptData(temperatureValue, encrypter),
+    bloodPressure: EncryptionClass().encryptData(bloodPressure, encrypter),
+    height: EncryptionClass().encryptData(heightValue, encrypter),
+    weight: EncryptionClass().encryptData(weightValue, encrypter),
+    hasSimilarCondition: EncryptionClass() .encryptData(hasSimilarConditionValue.toString(), encrypter),
+    cheifComplain: EncryptionClass().encryptData(cheifComplains.toString(), encrypter),
+    patientUndergoSurgery: EncryptionClass().encryptData(patientUndergoSurgeryValue, encrypter),
+    hasAllergies: EncryptionClass().encryptData(hasAllergiesValue, encrypter),
+    patientTakingMedication: EncryptionClass().encryptData(patientTakingMedicationValue, encrypter),
+    chiefComplainMessage: otherComplains != null && otherComplains.isNotEmpty ? EncryptionClass().encryptData(otherComplains, encrypter) : medicationComment,
+    patientTakingMedicationMessage: medicationComment != null && medicationComment.isNotEmpty ? EncryptionClass().encryptData(medicationComment, encrypter) : medicationComment,
+    status: 'Pending',
+    owner: patientUID,
+    createdBy: createdBy,
+  );
+
+  return model;
+}
+
+Future<PatientModel> encryptedPatient(WidgetRef ref, Encrypter encrypter, String patientUID, String createdBy, String filepath) async {
+  final patient = EncryptionClass().encryptData(ref.read(patientProvider).fullName, encrypter);
+  final gender = EncryptionClass().encryptData(ref.read(patientProvider).gender, encrypter);
+  final birthday = EncryptionClass().encryptData(ref.read(patientProvider).birthdate, encrypter);
+  final contactNumber = EncryptionClass().encryptData(ref.read(patientProvider).contactNumber, encrypter);
+  final schoolName = EncryptionClass().encryptData(ref.read(patientProvider).schoolName, encrypter);
+  final schoolId = EncryptionClass().encryptData(ref.read(patientProvider).schoolID, encrypter);
+
+  await cloud.createFile(
+      bucketId: Env.appwriteAvatarStorage,
+      fileId: patientUID,
+      file: InputFile.fromPath(path: "$filepath\\$patientUID.jpeg"));
+
+  PatientModel patientModel = PatientModel(
+      uid: patientUID,
+      fullName: patient,
+      gender: gender,
+      birthdate: birthday,
+      contactNumber: contactNumber,
+      schoolName: schoolName,
+      schoolID: schoolId,
+      avatar: '',
+      createdBy: createdBy)
+    ..createdAt = DateTime.now();
+
+  return patientModel;
+}
+
+uplloadToDB(WidgetRef ref, PatientModel patient, ScreeningModel screening) async {
+  String user = ref.read(userProvider).uid;
+  await database.createDocument(
+    databaseId: Env.appwriteDatabaseID,
+    collectionId: Env.appwritePatientCollection,
+    documentId: patient.uid,
+    data: {
+      "fullname": patient.fullName,
+      "gender": patient.gender,
+      "birthdate": patient.birthdate,
+      "contact_number": patient.contactNumber,
+      "school_id": patient.schoolID,
+      // "schools":
+      "nurse": user,
+      "created_at": DateTime.now().toIso8601String(),
+    },
+  );
+
+  await database.createDocument(databaseId: Env.appwriteDatabaseID, collectionId: Env.appwriteScreeningCollection, documentId: ID.unique(), data: {
+    "id": screening.uid,
+    "patients": patient.uid,
+    "nurse": user,
+    "created_at": DateTime.now().toIso8601String(),
+    "history_of_illness": screening.historyOfIllness,
+    "health_worker_comment": screening.healthWorkerComment,
+    "frame_of_interest": screening.frameOfInterest,
+    "temperature": screening.temperature,
+    "blood_pressure": screening.bloodPressure,
+    "height": screening.height,
+    "weight": screening.weight,
+    "has_similar_condition": screening.hasSimilarCondition,
+    "chief_complaint": screening.cheifComplain,
+    "chief_message": screening.chiefComplainMessage,
+    "has_allergies": screening.hasAllergies,
+    "taking_medication": screening.patientTakingMedication,
+    "medication_message": screening.patientTakingMedicationMessage,
+    // "doctors": screening.doctors,
+    "doctors_comment": screening.doctorsNote,
+    "status": screening.status,
+  });
 }
