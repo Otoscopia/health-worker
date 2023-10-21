48aeb2fe79e86c52af5063bbb1c38edc4f03f2c4
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 02:57:54 +0800

    feat: Material widget now requires patientList and screeningList params

--- lib/features/app/presentation/widgets/dashboard_widgets/table_widgets/material_widget.dart
+++ lib/features/app/presentation/widgets/dashboard_widgets/table_widgets/material_widget.dart
@@ -1,34 +1,30 @@
 import "package:fluent_ui/fluent_ui.dart";
 import "package:flutter/material.dart" as material;
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:intl/intl.dart";
 
 import "package:health_worker/core/exports.dart";
 import "package:health_worker/features/app/exports.dart";
 import "package:health_worker/features/app/presentation/pages/patient_record.dart";
-import "package:health_worker/features/app/presentation/providers/fetch_patient_provider.dart";
-import "package:health_worker/features/app/presentation/providers/fetch_screening_records_provider.dart";
-import "package:intl/intl.dart";
 
 class MaterialWidget extends ConsumerWidget {
-  const MaterialWidget({
-    super.key,
-  });
+  final List<PatientModel> patientList;
+  final List<ScreeningModel> screeningList;
+  const MaterialWidget(
+      {super.key, required this.patientList, required this.screeningList});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<PatientModel> patients = ref.watch(patientListProvider);
-    List<ScreeningModel> screenings = ref.watch(screeningListProvider);
     String filter = ref.watch(filterProvider);
 
-    List<Map<String, Object>> data = patients.map((patient) {
-      ScreeningModel screening =
-          screenings.where((element) => element.owner == patient.uid).first;
+    List<Map<String, Object>> data = patientList.map((patient) {
+      ScreeningModel? screening = screeningList.where((element) => element.owner == patient.uid).first;
       return {
         "patient": patient,
         "fullname": patient.fullName,
         "gender": patient.gender,
         "status": screening.status,
-        "uploadedDate": screening.uploadedAt,
+        "uploadedDate": screening.screenedAt,
       };
     }).toList();
 
@@ -37,25 +33,27 @@ class MaterialWidget extends ConsumerWidget {
     return material.Material(
       borderRadius: BorderRadius.circular(4),
       child: material.DataTable(
-          showCheckboxColumn: false,
-          border: TableBorder(borderRadius: BorderRadius.circular(4)),
-          columns: const [
-            material.DataColumn(label: Text(patientName)),
-            material.DataColumn(label: Text(genderLabel)),
-            material.DataColumn(label: Text(medicalRecordStatus)),
-            material.DataColumn(label: Text(uploadedDate)),
-          ],
-          rows: data.map((row) {
-            final PatientModel patient = row["patient"] as PatientModel;
-            return material.DataRow(cells: [
+        showCheckboxColumn: false,
+        border: TableBorder(borderRadius: BorderRadius.circular(4)),
+        columns: const [
+          material.DataColumn(label: Text(patientName)),
+          material.DataColumn(label: Text(genderLabel)),
+          material.DataColumn(label: Text(medicalRecordStatus)),
+          material.DataColumn(label: Text(uploadedDate)),
+        ],
+        rows: data.map((row) {
+          final PatientModel patient = row["patient"] as PatientModel;
+          return material.DataRow(
+            cells: [
               material.DataCell(Text(row["fullname"].toString())),
               material.DataCell(Text(row["gender"].toString())),
               material.DataCell(Text(row["status"].toString())),
               material.DataCell(Text(DateFormat("MMM dd, y").format(DateTime.parse(row["uploadedDate"].toString())))),
             ],
             onSelectChanged: (value) => Navigator.push(context, FluentPageRoute(builder: (context) => PatientRecord(patient: patient))),
-            );
-          }).toList()),
+          );
+        }).toList(),
+      ),
     );
   }
 }
