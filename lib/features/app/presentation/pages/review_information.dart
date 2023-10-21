c54176b9190f04f70d71e1f1cfb1505656957415
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-10-22 02:56:05 +0800

    chore: Hide scroll bar

--- lib/features/app/presentation/pages/review_information.dart
+++ lib/features/app/presentation/pages/review_information.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:health_worker/dependency_injection.dart';
 import 'package:health_worker/features/app/domain/usestate/post_medical_record.dart';
 import 'package:intl/intl.dart';
 import 'package:ionicons/ionicons.dart';
@@ -49,7 +50,7 @@ class ReviewInformation extends ConsumerWidget {
     final takingMedicationMessage =
         ref.read(screeningProvider).patientTakingMedicationMessage;
     final undergoSurgery = ref.read(screeningProvider).patientUndergoSurgery;
-    String filePath = ref.watch(screeningProvider).filePath;
+    String filePath = "${appDir.parent.path}\\${ref.read(patientProvider).uid}";
     final Directory dir = Directory(filePath);
     var leftImageFiles = dir
         .listSync()
@@ -77,142 +78,145 @@ class ReviewInformation extends ConsumerWidget {
       content: ContainerBox(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      MouseRegion(
-                        cursor: SystemMouseCursors.click,
-                        child: IconButton(
-                          style: ButtonStyle(iconSize: ButtonState.all(32)),
-                          icon: const Icon(Ionicons.close_outline),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ),
-                      largeWidth,
-                      revewTitle
-                    ],
-                  ),
-                  mediumHeight,
-                  const Divider(),
-                  largeHeight,
-                  const PatientInformationWidget(),
-                  largeHeight,
-                  Card(
-                    borderColor: transparent,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Row(
                       children: [
-                        const HealthRecordTitle(),
-                        mediumHeight,
-                        CustomRichText(title: performedBy, value: examiner),
-                        CustomRichText(
-                            title: performedAtLabel,
-                            value:
-                                DateFormat("MMM. dd, y").format(performedAt)),
-                        largeHeight,
-                        CustomRichText(
-                            title: historyOfIllness,
-                            value: historyOfIllnessComment),
-                        CustomRichText(
-                            title: complainsLabel, value: joinedComplains),
-                        if (generalComplain != null &&
-                            generalComplain.isNotEmpty)
-                          CustomRichText(
-                              title: generalComplains, value: generalComplain),
-                        largeHeight,
-                        CustomRichText(
-                            title: patientAllergyTitle,
-                            value: bool.parse(hasAllergies) ? "Yes" : "No"),
-                        CustomRichText(
-                            title: patientSimilarConditionTitle,
-                            value: bool.parse(similarCondition) ? "Yes" : "No"),
-                        CustomRichText(
-                            title: patientSurgicalTitle,
-                            value: bool.parse(undergoSurgery) ? "Yes" : "No"),
-                        CustomRichText(
-                            title: patientMedication,
-                            value: bool.parse(takingMedication) ? "Yes" : "No"),
-                        if (takingMedicationMessage != null &&
-                            takingMedicationMessage.isNotEmpty)
-                          CustomRichText(
-                              title: "Medication Status",
-                              value: takingMedicationMessage),
-                        largeHeight,
-                        CustomRichText(
-                            title: healthWorkerComment,
-                            value: healthWorkerCommentReview),
-                        CustomRichText(
-                            title: frameOfInterest, value: frameOfInteres),
-                        largeHeight,
-                        const Text("Left Ears Images").fontSize(16).bold(),
-                        SizedBox(
-                            height: 210,
-                            child: ResponsiveGridList(
-                                minItemsPerRow: 3,
-                                maxItemsPerRow: 6,
-                                minItemWidth: 250,
-                                children: leftImageFiles
-                                    .map((e) => Image.file(File(e.path)))
-                                    .toList())),
-                        largeHeight,
-                        const Text("Left Ears Videos (BUG)")
-                            .fontSize(16)
-                            .bold(),
-
-                        // SizedBox(
-                        //     height: 400,
-                        //     child: ResponsiveGridList(
-                        //         maxItemsPerRow: 3,
-                        //         minItemWidth: 250,
-                        //         children: leftVideoFiles
-                        //             .map((e) => VideoWidget(url: File(e.path)))
-                        //             .toList())),
-                        largeHeight,
-                        const Divider(),
-                        largeHeight,
-                        const Text("Right Ears").fontSize(16).bold(),
-                        largeHeight,
-                        SizedBox(
-                            height: 210,
-                            child: ResponsiveGridList(
-                                minItemsPerRow: 3,
-                                maxItemsPerRow: 6,
-                                minItemWidth: 250,
-                                children: rightImageFiles
-                                    .map((e) => Image.file(File(e.path)))
-                                    .toList())),
-                        const Text("Right Ears Videos (BUG)")
-                            .fontSize(16)
-                            .bold(),
-                        // SizedBox(
-                        //     height: 400,
-                        //     child: ResponsiveGridList(
-                        //         maxItemsPerRow: 3,
-                        //         minItemWidth: 250,
-                        //         children: rightVideoFiles
-                        //             .map((e) => VideoWidget(url: File(e.path)))
-                        //             .toList())),
-                        // mediumHeight,
-                        largeHeight,
-                        Center(
-                          child: FilledButton(
-                            child: const Text("Submit"),
-                            onPressed: () async {
-                              uploadMedicalRecord(ref).then((value) => Navigator.popUntil(context, (route) => route.isFirst));
+                        MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: IconButton(
+                            style: ButtonStyle(iconSize: ButtonState.all(32)),
+                            icon: const Icon(Ionicons.close_outline),
+                            onPressed: () {
+                              Navigator.pop(context);
                             },
                           ),
-                        )
+                        ),
+                        largeWidth,
+                        revewTitle
                       ],
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    mediumHeight,
+                    const Divider(),
+                    largeHeight,
+                    const PatientInformationWidget(),
+                    largeHeight,
+                    Card(
+                      borderColor: transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const HealthRecordTitle(),
+                          mediumHeight,
+                          CustomRichText(title: performedBy, value: examiner),
+                          CustomRichText(
+                              title: performedAtLabel,
+                              value:
+                                  DateFormat("MMM. dd, y").format(performedAt)),
+                          largeHeight,
+                          CustomRichText(
+                              title: historyOfIllness,
+                              value: historyOfIllnessComment),
+                          CustomRichText(
+                              title: complainsLabel, value: joinedComplains),
+                          if (generalComplain != null &&
+                              generalComplain.isNotEmpty)
+                            CustomRichText(
+                                title: generalComplains, value: generalComplain),
+                          largeHeight,
+                          CustomRichText(
+                              title: patientAllergyTitle,
+                              value: bool.parse(hasAllergies) ? "Yes" : "No"),
+                          CustomRichText(
+                              title: patientSimilarConditionTitle,
+                              value: bool.parse(similarCondition) ? "Yes" : "No"),
+                          CustomRichText(
+                              title: patientSurgicalTitle,
+                              value: bool.parse(undergoSurgery) ? "Yes" : "No"),
+                          CustomRichText(
+                              title: patientMedication,
+                              value: bool.parse(takingMedication) ? "Yes" : "No"),
+                          if (takingMedicationMessage != null &&
+                              takingMedicationMessage.isNotEmpty)
+                            CustomRichText(
+                                title: "Medication Status",
+                                value: takingMedicationMessage),
+                          largeHeight,
+                          CustomRichText(
+                              title: healthWorkerComment,
+                              value: healthWorkerCommentReview),
+                          CustomRichText(
+                              title: frameOfInterest, value: frameOfInteres),
+                          largeHeight,
+                          const Text("Left Ears Images").fontSize(16).bold(),
+                          SizedBox(
+                              height: 210,
+                              child: ResponsiveGridList(
+                                  minItemsPerRow: 3,
+                                  maxItemsPerRow: 6,
+                                  minItemWidth: 250,
+                                  children: leftImageFiles
+                                      .map((e) => Image.file(File(e.path)))
+                                      .toList())),
+                          largeHeight,
+                          const Text("Left Ears Videos (BUG)")
+                              .fontSize(16)
+                              .bold(),
+          
+                          // SizedBox(
+                          //     height: 400,
+                          //     child: ResponsiveGridList(
+                          //         maxItemsPerRow: 3,
+                          //         minItemWidth: 250,
+                          //         children: leftVideoFiles
+                          //             .map((e) => VideoWidget(url: File(e.path)))
+                          //             .toList())),
+                          largeHeight,
+                          const Divider(),
+                          largeHeight,
+                          const Text("Right Ears").fontSize(16).bold(),
+                          largeHeight,
+                          SizedBox(
+                              height: 210,
+                              child: ResponsiveGridList(
+                                  minItemsPerRow: 3,
+                                  maxItemsPerRow: 6,
+                                  minItemWidth: 250,
+                                  children: rightImageFiles
+                                      .map((e) => Image.file(File(e.path)))
+                                      .toList())),
+                          const Text("Right Ears Videos (BUG)")
+                              .fontSize(16)
+                              .bold(),
+                          // SizedBox(
+                          //     height: 400,
+                          //     child: ResponsiveGridList(
+                          //         maxItemsPerRow: 3,
+                          //         minItemWidth: 250,
+                          //         children: rightVideoFiles
+                          //             .map((e) => VideoWidget(url: File(e.path)))
+                          //             .toList())),
+                          // mediumHeight,
+                          largeHeight,
+                          Center(
+                            child: FilledButton(
+                              child: const Text("Submit"),
+                              onPressed: () async {
+                                uploadMedicalRecord(ref).then((value) => Navigator.popUntil(context, (route) => route.isFirst));
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
