3df8adb6e4b8b7a2b94406801342481c63e6916e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 03:10:58 +0800

    feat: Create assignment entity

---
+++ lib/core/entities/assignment_entity.dart
@@ -0,0 +1,15 @@
+class AssignmentEntity {
+  final String id;
+  final String nurse;
+  final String school;
+  final String startDate;
+  final String endDate;
+
+  AssignmentEntity({
+    required this.id,
+    required this.nurse,
+    required this.school,
+    required this.startDate,
+    required this.endDate,
+  });
+}
