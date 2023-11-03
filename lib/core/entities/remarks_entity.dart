a5ee8e59ce5c0e830e14855bc54d2cb4a4ee3259
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 03:11:26 +0800

    feat: Create remarks entity

---
+++ lib/core/entities/remarks_entity.dart
@@ -0,0 +1,14 @@
+
+class RemarksEntity {
+  final String id;
+  final String screening;
+  final String followUpDate;
+  final String remarks;
+
+  RemarksEntity({
+    required this.id,
+    required this.screening,
+    required this.followUpDate,
+    required this.remarks,
+  });
+}
