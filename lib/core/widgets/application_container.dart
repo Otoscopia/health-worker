ac598feaa441870f1240e6c1c89bdb03a5c1d91a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:37:38 +0800

    feat: Create application container that warps arround scaffold page and offline builder

---
+++ lib/core/widgets/application_container.dart
@@ -0,0 +1,30 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_offline/flutter_offline.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:health_worker/core/core.dart';
+
+class ApplicationContainer extends ConsumerWidget {
+  final Widget child;
+  const ApplicationContainer({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ScaffoldPage(
+      padding: noPadding,
+      content: OfflineBuilder(
+        connectivityBuilder: (context, value, child) {
+          final bool connected = value != ConnectivityResult.none;
+
+          return Stack(
+            fit: StackFit.expand,
+            children: [child, OfflineBar(connected: connected)],
+          );
+        },
+        child: Column(
+          children: [Expanded(child: child)],
+        ),
+      ),
+    );
+  }
+}
