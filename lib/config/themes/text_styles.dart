600e01661982ccc7d2ec3e25d435ec318d932f73
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:00:18 +0800

    feat: Create application default text style

---
+++ lib/config/themes/text_styles.dart
@@ -0,0 +1,90 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+import 'package:health_worker/config/config.dart';
+
+class LightTextStyles {
+  final display = TextStyle(
+    fontSize: 72,
+    color: darkTextColor,
+    fontWeight: FontWeight.w600,
+  );
+
+  final titleLarge = TextStyle(
+    fontSize: 44,
+    color: darkTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final title = TextStyle(
+    fontSize: 32,
+    color: darkTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final subtitle = TextStyle(
+    fontSize: 24,
+    color: darkTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final bodyLarge = TextStyle(
+    fontSize: 22,
+    color: darkTextColor,
+    fontWeight: FontWeight.normal,
+  );
+  final bodyStrong = TextStyle(
+    fontSize: 18,
+    color: darkTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final body = TextStyle(
+    fontSize: 18,
+    color: darkTextColor,
+    fontWeight: FontWeight.normal,
+  );
+  final caption = TextStyle(
+    fontSize: 16,
+    color: darkTextColor,
+    fontWeight: FontWeight.normal,
+  );
+}
+
+class DarkTextStyles {
+  final display = TextStyle(
+    fontSize: 72,
+    color: lightTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final titleLarge = TextStyle(
+    fontSize: 44,
+    color: lightTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final title = TextStyle(
+    fontSize: 32,
+    color: lightTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final subtitle = TextStyle(
+    fontSize: 24,
+    color: lightTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final bodyLarge = TextStyle(
+    fontSize: 22,
+    color: lightTextColor,
+    fontWeight: FontWeight.normal,
+  );
+  final bodyStrong = TextStyle(
+    fontSize: 18,
+    color: lightTextColor,
+    fontWeight: FontWeight.w600,
+  );
+  final body = TextStyle(
+    fontSize: 18,
+    color: lightTextColor,
+    fontWeight: FontWeight.normal,
+  );
+  final caption = TextStyle(
+    fontSize: 16,
+    color: lightTextColor,
+    fontWeight: FontWeight.normal,
+  );
+}
