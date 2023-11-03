6bd870e0a1cb3b79d2058b862397ccbe7172bb96
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 03:11:36 +0800

    feat: Create school entity

---
+++ lib/core/entities/school_entity.dart
@@ -0,0 +1,13 @@
+class SchoolEntity {
+  final String id;
+  final String name;
+  final String code;
+  final String address;
+
+  SchoolEntity({
+    required this.id,
+    required this.name,
+    required this.code,
+    required this.address,
+  });
+}
