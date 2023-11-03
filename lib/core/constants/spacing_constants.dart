eaf9b7ea8010e070b99089da742cf5f1827cd7f5
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:36:43 +0800

    feat: Create sizebox for application spacing

---
+++ lib/core/constants/spacing_constants.dart
@@ -0,0 +1,14 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+const smallWidth = SizedBox(width: 4);
+const mediumWidth = SizedBox(width: 8);
+const largeWidth = SizedBox(width: 16);
+
+const smallHeight = SizedBox(height: 4);
+const mediumHeight = SizedBox(height: 8);
+const largeHeight = SizedBox(height: 16);
+
+const noPadding = EdgeInsets.all(0);
+const smallPadding = EdgeInsets.all(4);
+const mediumPadding = EdgeInsets.all(8);
+const largePadding = EdgeInsets.all(16);
