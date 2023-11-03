fb3aa8b9d74f6543d97421a3a3c54b1502d38bf4
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 04:09:34 +0800

    feat: Export app, authentication and navigation feature

---
+++ lib/features/features.dart
@@ -0,0 +1,8 @@
+// App Directory
+export './app/app.dart';
+
+// Authentication Directory
+export './authentication/authentication.dart';
+
+// Navigation Directory
+export './navigation/navigation.dart';
