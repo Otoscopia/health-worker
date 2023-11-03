ac0144b15b99964186a88c0281bcfd5405da16b7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:53:59 +0800

    feat: Export entities, repositories and use cases directory

---
+++ lib/features/authentication/domain/domain.dart
@@ -0,0 +1,12 @@
+// Entities Directory
+export './entities/authentication_entity.dart';
+export 'entities/auth_state_entity.dart';
+export './entities/user_entity.dart';
+
+// Repositories Directory
+export './repositories/authentication_repository.dart';
+
+// Use Cases Directory
+export './use_cases/get_current_user_use_case.dart';
+export './use_cases/sign_in_use_case.dart';
+export './use_cases/sign_out_use_case.dart';
