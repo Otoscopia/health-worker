46c3cdfffda159dd8a29274473e532e7771918a0
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:38:59 +0800

    feat: Create text widget that will house all text related widget

---
+++ lib/core/widgets/text_widget.dart
@@ -0,0 +1,15 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+import 'package:health_worker/config/config.dart';
+
+class PrimaryCaption extends ConsumerWidget {
+  final String text;
+  const PrimaryCaption({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Text(text, style: FluentTheme.of(context).typography.body).textColor(primary);
+  }
+}
