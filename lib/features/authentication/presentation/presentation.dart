1acf134a8f843d1f3cffb0584993c6e64339fe35
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 15:27:24 +0800

    feat: Export providers, screens, and widgets directory

---
+++ lib/features/authentication/presentation/presentation.dart
@@ -0,0 +1,16 @@
+// Providers Directory
+export 'providers/authentication_provider.dart';
+export 'providers/email_provider.dart';
+export 'providers/auth_state_provider.dart';
+export 'providers/password_provider.dart';
+
+// Screen Directory
+export 'screens/sign_in.dart';
+
+// Widgets Directory
+export 'widgets/application_brand_container.dart';
+export 'widgets/email_input.dart';
+export 'widgets/form_container.dart';
+export 'widgets/password_input.dart';
+export 'widgets/sign_in_button.dart';
+
