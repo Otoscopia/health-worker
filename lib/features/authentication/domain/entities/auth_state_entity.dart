4556d5a78aafea38bb8301fc89df765d1136985e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:53:36 +0800

    feat: Create auth state entity

---
+++ lib/features/authentication/domain/entities/auth_state_entity.dart
@@ -0,0 +1,7 @@
+class AuthStateEntity {
+  final bool loading;
+  final bool error;
+  final String message;
+
+  AuthStateEntity({required this.loading, required this.error, required this.message});
+}
