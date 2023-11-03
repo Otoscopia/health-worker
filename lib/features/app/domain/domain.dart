59abcf2b2fe07dd80342de2ffdeb181078af0b59
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 07:19:05 +0800

    feat: Export entity, repositories and domain

---
+++ lib/features/app/domain/domain.dart
@@ -0,0 +1,19 @@
+// Entity Directory
+export 'entities/assignment_entity.dart';
+export 'entities/patient_entity.dart';
+export 'entities/remarks_entity.dart';
+export 'entities/school_entity.dart';
+export 'entities/screening_entity.dart';
+export 'entities/user_entity.dart';
+
+// Repositories Directory
+export 'repositories/application_repository.dart';
+
+// Use Cases Directory
+export 'use_cases/get_assignments_use_case.dart';
+export 'use_cases/get_doctors_use_case.dart';
+export 'use_cases/get_patients_use_case.dart';
+export 'use_cases/get_remarks_use_case.dart';
+export 'use_cases/get_schools_use_case.dart';
+export 'use_cases/get_screening_use_case.dart';
+export 'use_cases/get_user_document.dart';
