f872d42956ed85f516c8d587ed8147290a7d96e8
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:16:31 +0800

    feat: Export model, presentation, util, and widget directory

---
+++ lib/core/core.dart
@@ -0,0 +1,12 @@
+// Model Directory
+export './models/env_model.dart';
+
+// Presentation Directory
+export './presentation/screen/navigation.dart';
+
+// Util Directory
+export './util/dependency_injection.dart';
+
+// Widgets Directory
+export './widgets/responsive_builder.dart';
+
