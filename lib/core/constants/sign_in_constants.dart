60c26f6af7331f02804f8df3880258c51037ff47
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-03 14:36:12 +0800

    feat: Create sign in constants

---
+++ lib/core/constants/sign_in_constants.dart
@@ -0,0 +1,13 @@
+const String emailAddressText = 'Email Address';
+const String emailAddressLabel = emailAddressText;
+const String emailAddressError = 'Please enter valid email address';
+
+const String passwordText = 'Password';
+const String passwordLabel = passwordText;
+const String passwordError = 'Please enter a valid password';
+
+const String forgotPassword = 'Forgot password?';
+
+const String signInText = 'Sign In';
+
+const String signInErrorLabel = 'Ohh ohh!';
