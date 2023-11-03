4a3a5f9e9238a96ae9cae0244deccda14c033dfc
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 04:33:51 +0800

    chore: Add more export

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,13 +1,25 @@
 // Constants Directory
 export 'constants/constants.dart';
+export 'constants/dashboard_constants.dart';
 export 'constants/sign_in_constants.dart';
 export 'constants/spacing_constants.dart';
 
-// Model Directory
-export 'models/env_model.dart';
+// Entity Directory
+export 'entities/assignment_entity.dart';
+export 'entities/patient_entity.dart';
+export 'entities/remarks_entity.dart';
+export 'entities/school_entity.dart';
+export 'entities/screening_entity.dart';
+export 'entities/user_entity.dart';
 
-// Presentation Directory
-export 'presentation/screen/navigation.dart';
+// Model Directory
+export 'models/assignment_model.dart';
+export 'models/env_model.dart';
+export 'models/patient_model.dart';
+export 'models/remarks_model.dart';
+export 'models/school_model.dart';
+export 'models/screening_model.dart';
+export 'models/user_model.dart';
 
 // Util Directory
 export 'util/dependency_injection.dart';
