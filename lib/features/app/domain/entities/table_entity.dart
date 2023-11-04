355ef42e0c2efffb78f48b2231bc53ee0ef5062a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 08:53:38 +0800

    feat: Create table entity

---
+++ lib/features/app/domain/entities/table_entity.dart
@@ -0,0 +1,17 @@
+class TableEntity {
+  final String name;
+  final String age;
+  final String gender;
+  final String status;
+  final String doctor;
+  final String school;
+
+  TableEntity({
+    required this.name,
+    required this.age,
+    required this.gender,
+    required this.status,
+    required this.doctor,
+    required this.school,
+  });
+}
