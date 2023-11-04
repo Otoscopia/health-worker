e7d93d02cd2a20182e08d7ba4fcb2fc5d56280ff
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 08:31:25 +0800

    feat: Create dashboard screen

---
+++ lib/features/app/presentation/screens/dashboard.dart
@@ -0,0 +1,31 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:health_worker/core/core.dart';
+import 'package:health_worker/features/features.dart';
+
+class Dashboard extends ConsumerWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ApplicationContainer(
+      child: FutureBuilder(
+        future: Future.wait([
+          ref.watch(futureAssignmentProvider.future),
+          ref.watch(futureDoctorsProvider.future),
+          ref.watch(futureSchoolsProvider.future),
+        ]),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorScreen(error: snapshot.error);
+          } else if (snapshot.hasData) {
+            return const DashboardNavigation();
+          }
+
+          return const LoadingAnimation();
+        },
+      ),
+    );
+  }
+}
