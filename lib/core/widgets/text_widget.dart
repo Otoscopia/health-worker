318f591e194e4123ea938a69825caef91cf4852e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 13:11:53 +0800

    feat: Create text subtitle

--- lib/core/widgets/text_widget.dart
+++ lib/core/widgets/text_widget.dart
@@ -6,10 +6,20 @@ import 'package:health_worker/config/config.dart';
 
 class PrimaryCaption extends ConsumerWidget {
   final String text;
-  const PrimaryCaption({super.key, required this.text});
+  const PrimaryCaption(this.text, {super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Text(text, style: FluentTheme.of(context).typography.body).textColor(primary);
+  }
+}
+
+class TextSubtitle extends ConsumerWidget {
+  final String text;
+  const TextSubtitle(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Text(text, style: FluentTheme.of(context).typography.subtitle);
   }
 }
