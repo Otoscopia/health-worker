141e68adfd8d6b1ca6f4526f8129729c4a6d711d
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 13:13:54 +0800

    feat: Create home constants

---
+++ lib/core/constants/home_constants.dart
@@ -0,0 +1,8 @@
+const String tableColumnName = "Name";
+const String tableColumnAge = "Age";
+const String tableColumnGender = "Gender";
+const String tableColumnStatus = "Status";
+const String tableColumnDoctor = "Doctor";
+const String tableColumnSchool = "School";
+
+const String noRecord = "No Record found";
