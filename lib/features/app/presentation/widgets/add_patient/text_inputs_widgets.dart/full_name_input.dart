f7a0b8066b6814cc65453b2cacd4bdeb96b1a5aa
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 14:50:54 +0800

    feat: Create full name input

---
+++ lib/features/app/presentation/widgets/add_patient/text_inputs_widgets.dart/full_name_input.dart
@@ -0,0 +1,36 @@
+import "package:flutter/services.dart";
+
+import "package:fluent_ui/fluent_ui.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "package:health_worker/config/config.dart";
+import "package:health_worker/core/core.dart";
+import "package:health_worker/features/features.dart";
+
+
+class FullNameInput extends ConsumerWidget {
+  const FullNameInput({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InfoLabel(
+      label: fullNameLabel,
+      child: TextFormBox(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        placeholder: fullNamePlaceholder,
+        inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z .]'))],
+        maxLength: 50,
+        maxLines: 1,
+        maxLengthEnforcement: MaxLengthEnforcement.enforced,
+        unfocusedColor: transparent,
+        onChanged: (value) => ref.read(fullnameProvider.notifier).setFullname(value),
+        validator: (value) {
+          if (value == null || value.isEmpty || value.length <= 5) {
+            return fullNameError;
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
