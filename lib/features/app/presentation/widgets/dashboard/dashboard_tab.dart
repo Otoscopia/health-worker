e1eed62d3521ae7621b7721270e829fdb3190382
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 08:37:21 +0800

    feat: Create dashboard tab

---
+++ lib/features/app/presentation/widgets/dashboard/dashboard_tab.dart
@@ -0,0 +1,24 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:health_worker/core/core.dart';
+import 'package:health_worker/features/app/presentation/providers/dashboard_tab_provider.dart';
+
+class DashboardTab extends ConsumerWidget {
+  const DashboardTab({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return TabView(
+      closeButtonVisibility: CloseButtonVisibilityMode.always,
+      closeDelayDuration: Duration.zero,
+      currentIndex: 0,
+      onChanged: (index) => ref.read(dashboardTabProvider.notifier).setTab(index),
+      onReorder: (oldIndex, newIndex) => ref.read(dashboardTabProvider.notifier).setTabOrder(oldIndex, newIndex),
+      shortcutsEnabled: true,
+      showScrollButtons: true,
+      tabs: dashboardTabs,
+      tabWidthBehavior: TabWidthBehavior.equal,
+    );
+  }
+}
