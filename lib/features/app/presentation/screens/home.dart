0e59a5ff6812f6b0f56329a610b66f21df10d8ea
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 12:54:51 +0800

    feat: Create home screen

---
+++ lib/features/app/presentation/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:health_worker/features/features.dart';
+
+class Home extends ConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Card(
+      child: Stack(
+        children: [
+          HomeTable(),
+          FloatingActionButton(
+            icon: Icon(FluentIcons.add),
+            route: AddPatientInformation(),
+          )
+        ],
+      ),
+    );
+  }
+}
