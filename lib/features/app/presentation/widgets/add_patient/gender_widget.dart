b658c89f4393092b3ced9be481cd9131890220e4
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-04 14:52:15 +0800

    feat: Create gender radio buttons

---
+++ lib/features/app/presentation/widgets/add_patient/gender_widget.dart
@@ -0,0 +1,34 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:health_worker/core/core.dart';
+import 'package:health_worker/features/features.dart';
+
+class GenderWidget extends ConsumerWidget {
+  const GenderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InfoLabel(
+      label: genderTitle,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List.generate(
+          genders.length,
+          (index) {
+            return RadioButton(
+              checked: ref.watch(genderProvider) == index,
+              content: Text(genders[index]),
+              onChanged: (checked) {
+                if (checked) {
+                  ref.watch(genderProvider.notifier).setGender(index);
+                  ref.watch(genderErrorProvider.notifier).setGenderError(false);
+                }
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
