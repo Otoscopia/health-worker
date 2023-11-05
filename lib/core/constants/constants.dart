33a21fbe3deae545c2b13be979bb0c526fdaf67a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-05 09:48:02 +0800

    feat: Add prceed, cancel, continue and close icon constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,16 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:ionicons/ionicons.dart';
+
 const String logoSvg = 'assets/logo.svg';
 const String otoscopia = 'assets/otoscopia-text.svg';
 
 const String applicationTitle = "Otoscopia ENT Website";
 
 const String offlineText = "You are offline";
+
+const String proceedLabel = "Proceed";
+const String cancelLabel = "Cancel";
+
+const String continueLabel = "Continue";
+
+const Icon closeButtonIcon = Icon(Ionicons.close_outline);
