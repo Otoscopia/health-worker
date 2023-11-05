60ed2e1a28f1d4377e9f1e98e722566ebe52a330
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-05 15:00:34 +0800

    feat: Export all directory in presentation

---
+++ lib/features/app/presentation/presentation.dart
@@ -0,0 +1,81 @@
+export 'screens/account_information.dart';
+export 'screens/add_patient.dart';
+export 'screens/dashboard.dart';
+export 'screens/doctors.dart';
+export 'screens/home.dart';
+// export 'screens/left_camera.dart';
+export 'screens/patient_record.dart';
+export 'screens/patients.dart';
+export 'screens/review_information.dart';
+// export 'screens/right_camera.dart';
+export 'screens/schools.dart';
+export 'screens/screening.dart';
+export 'screens/settings.dart';
+
+export 'widgets/dashboard/application_header.dart';
+export 'widgets/dashboard/dashboard_navigation.dart';
+export 'widgets/dashboard/dashboard_tab.dart';
+export 'widgets/dashboard/search_box.dart';
+export 'widgets/dashboard/user_container.dart';
+
+export 'widgets/home/floating_action_button.dart';
+export 'widgets/home/home_table.dart';
+export 'widgets/home/home_table_source.dart';
+
+export 'widgets/add_patient/add_patient_button.dart';
+export 'widgets/add_patient/birthdate_widget.dart';
+export 'widgets/add_patient/birthday_error.dart';
+export 'widgets/add_patient/gender_error.dart';
+export 'widgets/add_patient/gender_widget.dart';
+export 'widgets/add_patient/text_inputs_widgets.dart/full_name_input.dart';
+export 'widgets/add_patient/text_inputs_widgets.dart/guardians_name.dart';
+export 'widgets/add_patient/text_inputs_widgets.dart/guarians_phone_number.dart';
+export 'widgets/add_patient/text_inputs_widgets.dart/school_id_input.dart';
+export 'widgets/add_patient/text_inputs_widgets.dart/school_name_input.dart';
+
+export 'widgets/screening/cheif_complain.dart';
+export 'widgets/screening/cheif_complain_error.dart';
+export 'widgets/screening/cheif_complain_widget.dart';
+export 'widgets/screening/examiner_and_date.dart';
+export 'widgets/screening/height_and_weight.dart';
+export 'widgets/screening/illness_and_comment.dart';
+export 'widgets/screening/medication_widget.dart';
+export 'widgets/screening/other_complain.dart';
+export 'widgets/screening/patient_allergies_widget.dart';
+export 'widgets/screening/patient_have_allergies.dart';
+export 'widgets/screening/patient_medication_widget.dart';
+export 'widgets/screening/patient_similar_condition.dart';
+export 'widgets/screening/patient_taking_medication.dart';
+export 'widgets/screening/patient_undergo_surgery.dart';
+export 'widgets/screening/patient_undergo_surgery_widget.dart';
+export 'widgets/screening/similar_condition_error.dart';
+export 'widgets/screening/similar_condition_widget.dart';
+export 'widgets/screening/temperature_and_blood.dart';
+
+export 'widgets/title_widget.dart';
+
+export 'providers/add_patient_provider.dart';
+export 'providers/dashboard_tab_provider.dart';
+export 'providers/dashboard_future_providers.dart';
+export 'providers/dashboard_page_provider.dart';
+export 'providers/future_assignment_provider.dart';
+export 'providers/future_doctor_provider.dart';
+export 'providers/future_patient_provider.dart';
+export 'providers/future_schools_provider.dart';
+export 'providers/future_user_provider.dart';
+
+export 'providers/add_patient_providers/birthdate_provider.dart';
+export 'providers/add_patient_providers/contact_number_provider.dart';
+export 'providers/add_patient_providers/fullname_provider.dart';
+export 'providers/add_patient_providers/gender_provider.dart';
+export 'providers/add_patient_providers/guardian_fullname_provider.dart';
+export 'providers/add_patient_providers/school_id.dart';
+export 'providers/add_patient_providers/school_name_provider.dart';
+
+export 'providers/screening/cheif_complain.dart';
+export 'providers/screening/complain_provider.dart';
+export 'providers/screening/height_weight_provider.dart';
+export 'providers/screening/history_and_comment.dart';
+export 'providers/screening/medication_provider.dart';
+export 'providers/screening/similar_condition_and_allergies.dart';
+export 'providers/screening/temperature_and_blood_provider.dart';
