5604cb12e5d23c87051678d4f604cd19ec662a5e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-05 14:58:19 +0800

    feat: Create screening provider

---
+++ lib/features/app/presentation/providers/add_screening_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:health_worker/core/core.dart';
+import 'package:health_worker/features/app/app.dart';
+
+class ScreeningNotifier extends StateNotifier<ScreeningEntity> {
+  ScreeningNotifier() : super(emptyScreening);
+
+  setScreening(ScreeningEntity screening) {
+    final AddScreeningLocalUseCase local = AddScreeningLocalUseCase(repository: applicationRepository);
+    local.execute(screening: screening);
+    
+    // final AddScreeningRemoteUseCase remote = AddScreeningRemoteUseCase(repository: applicationRepository);
+    // remote.execute(screening: screening);
+
+    state = screening;
+  }
+}
+
+final screeningProvider =
+    StateNotifierProvider<ScreeningNotifier, ScreeningEntity>((ref) {
+  return ScreeningNotifier();
+});
+
+ScreeningEntity emptyScreening = ScreeningEntity(
+    id: "",
+    patient: "",
+    assignment: "",
+    historyOfIllness: "",
+    healthWorkerRemarks: "",
+    temperature: "",
+    height: "",
+    weight: "",
+    hasSimilarCondition: "",
+    chiefComplaint: "",
+    chiefComplaintMessage: "",
+    hasAllergies: "",
+    typeOfAllergies: "",
+    undergoSurgery: "",
+    takingMedication: "",
+    takingMedicationMessage: "",
+    status: "");
