ec6065b75af0a60bb75d310ce4ab7be48dd29287
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-05 14:58:43 +0800

    feat: Save assignment to assignment provider

--- lib/features/app/presentation/providers/future_assignment_provider.dart
+++ lib/features/app/presentation/providers/future_assignment_provider.dart
@@ -6,5 +6,16 @@ import 'package:health_worker/features/features.dart';
 final futureAssignmentProvider = FutureProvider<List<AssignmentEntity>>((ref) async {
   final assginmentUseCase = GetRemoteAssignmentUseCase(repository: applicationRepository);
   final assignments = await assginmentUseCase.execute();
+  ref.read(assignmentProvider.notifier).setAssignment(assignments);
   return assignments;
+});
+
+class AssignmentNotifier extends StateNotifier<List<AssignmentEntity>> {
+  AssignmentNotifier() : super([]);
+
+  setAssignment(List<AssignmentEntity> assignment) => state = assignment;
+}
+
+final assignmentProvider = StateNotifierProvider<AssignmentNotifier, List<AssignmentEntity>>((ref) {
+  return AssignmentNotifier();
 });
