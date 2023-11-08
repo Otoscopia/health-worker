fed7bc6f457c8013f45842ec81e5707b43eea645
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-09 04:48:23 +0800

    Add new data sources, models, repositories, and use cases for various entities

--- lib/features/app/data/data.dart
+++ lib/features/app/data/data.dart
@@ -1,10 +1,24 @@
+export 'data_sources/application_data_source.dart';
 export 'data_sources/encryption_data_source.dart';
-export 'data_sources/local/local_get_database_data_source.dart';
-export 'data_sources/local/local_set_database_data_source.dart';
-export 'data_sources/remote/remote_get_data_source.dart';
-export 'data_sources/remote/remote_set_data_source.dart';
+
+export 'data_sources/local/local_assignments_data_source.dart';
+export 'data_sources/local/local_doctors_data_source.dart';
+export 'data_sources/local/local_patients_data_source.dart';
+export 'data_sources/local/local_remarks_data_sources.dart';
+export 'data_sources/local/local_schools_data_source.dart';
+export 'data_sources/local/local_screenings_data_source.dart';
+export 'data_sources/local/local_user_data_source.dart';
+
+export 'data_sources/remote/remote_assignments_data_source.dart';
+export 'data_sources/remote/remote_doctors_data_source.dart';
+export 'data_sources/remote/remote_patients_data_source.dart';
+export 'data_sources/remote/remote_remarks_data_source.dart';
+export 'data_sources/remote/remote_schools_data_source.dart';
+export 'data_sources/remote/remote_screenings_data_source.dart';
+export 'data_sources/remote/remote_user_data_source.dart';
 
 // Model Directory
+export 'models/application_model.dart';
 export 'models/assignment_model.dart';
 export 'models/doctor_model.dart';
 export 'models/patient_model.dart';
@@ -15,3 +29,11 @@ export 'models/user_model.dart';
 
 // Repository Directory
 export 'repositories/application_repository_impl.dart';
+export 'repositories/assignments_repository_impl.dart';
+export 'repositories/doctors_repository_impl.dart';
+export 'repositories/encryption_repository_impl.dart';
+export 'repositories/patients_repository_impl.dart';
+export 'repositories/remarks_repository_impl.dart';
+export 'repositories/schools_repository_impl.dart';
+export 'repositories/screenings_repository_impl.dart';
+export 'repositories/user_repository_impl.dart';
--- lib/features/app/domain/domain.dart
+++ lib/features/app/domain/domain.dart
@@ -1,4 +1,5 @@
 // Entity Directory
+export 'entities/application_entity.dart';
 export 'entities/assignment_entity.dart';
 export 'entities/patient_entity.dart';
 export 'entities/remarks_entity.dart';
@@ -10,14 +11,21 @@ export 'entities/user_entity.dart';
 
 // Repositories Directory
 export 'repositories/application_repository.dart';
+export 'repositories/assignments_repository.dart';
+export 'repositories/doctors_repository.dart';
+export 'repositories/encryption_repository.dart';
+export 'repositories/patients_repository.dart';
+export 'repositories/remarks_repository.dart';
+export 'repositories/schools_repository.dart';
+export 'repositories/screenings_repository.dart';
+export 'repositories/user_repository.dart';
 
 // Use Cases Directory
-export 'use_cases/add_patient_use_case.dart';
-export 'use_cases/add_screening_use_case.dart';
-export 'use_cases/get_assignments_use_case.dart';
-export 'use_cases/get_doctors_use_case.dart';
-export 'use_cases/get_patients_use_case.dart';
-export 'use_cases/get_remarks_use_case.dart';
-export 'use_cases/get_schools_use_case.dart';
-export 'use_cases/get_screening_use_case.dart';
-export 'use_cases/get_user_document.dart';
+export 'use_cases/assignments_use_case.dart';
+export 'use_cases/doctors_use_cases.dart';
+export 'use_cases/encryption_service_use_cases.dart';
+export 'use_cases/patients_use_cases.dart';
+export 'use_cases/remarks_use_case.dart';
+export 'use_cases/schools_use_cases.dart';
+export 'use_cases/screenings_use_cases.dart';
+export 'use_cases/user_use_cases.dart';
