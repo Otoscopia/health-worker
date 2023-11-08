23a10fde3a934d600a8b034428b30a5a5b7ea3d9
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-09 04:48:39 +0800

    feat: Add use cases for repositories

---
+++ lib/features/app/domain/use_cases/use_cases.dart
@@ -0,0 +1,42 @@
+import 'package:health_worker/features/app/app.dart';
+
+class UseCases {
+  final UserRepository _userRepository;
+  final ScreeningsRepository _screeningsRepository;
+  final SchoolsRepository _schoolsRepository;
+  final RemarksRepository _remarksRepository;
+  final PatientsRepository _patientsRepository;
+  final DoctorsRepository _doctorsRepository;
+  final AssignmentsRepository _assignment;
+
+  UseCases({
+    required UserRepository userRepository,
+    required ScreeningsRepository screeningsRepository,
+    required SchoolsRepository schoolsRepository,
+    required RemarksRepository remarksRepository,
+    required PatientsRepository patientsRepository,
+    required DoctorsRepository doctorsRepository,
+    required AssignmentsRepository assignmentsRepository
+  })  : _userRepository = userRepository,
+        _screeningsRepository = screeningsRepository,
+        _schoolsRepository = schoolsRepository,
+        _remarksRepository = remarksRepository,
+        _patientsRepository = patientsRepository,
+        _doctorsRepository = doctorsRepository,
+        _assignment = assignmentsRepository;
+  
+  // User use cases
+  AssignmentsUseCase get assignmentsUseCase => AssignmentsUseCase(repository: _assignment);
+
+  DoctorsUseCases get doctorsUseCases => DoctorsUseCases(repository: _doctorsRepository);
+
+  PatientsUseCases get patientsUseCases => PatientsUseCases(repository: _patientsRepository);
+
+  RemarksUseCases get remarksUseCases => RemarksUseCases(repository: _remarksRepository);
+
+  SchoolsUseCase get schoolsUseCase => SchoolsUseCase(repository: _schoolsRepository);
+
+  ScreeningsUseCases get screeningsUseCases => ScreeningsUseCases(repository: _screeningsRepository);
+
+  UserUseCases get userUseCases => UserUseCases(repository: _userRepository);
+}
