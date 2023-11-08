59f20381071ea4418c8f769a958b68a29d3aacc4
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-09 04:47:21 +0800

    feat: Add local and remote data sources and repository implementation for patients feature

---
+++ lib/features/app/domain/use_cases/patients_use_cases.dart
@@ -0,0 +1,38 @@
+import 'package:health_worker/features/features.dart';
+
+class PatientsUseCases {
+  final PatientsRepository _repository;
+
+  PatientsUseCases({required PatientsRepository repository})
+      : _repository = repository;
+
+  // Get patients from remote database using patient repository
+  Future<List<PatientEntity>> getRemotePatients() async {
+    return await _repository.getRemotePatients();
+  }
+
+  // Get patients from local database using patient repository
+  Future<List<PatientEntity>> getLocalPatients() async {
+    return await _repository.getLocalPatients();
+  }
+
+  // Set patients to local database using patient repository
+  Future<void> setPatients(List<PatientEntity> patients) async {
+    return await _repository.setPatients(patients: patients);
+  }
+
+  // Set patient to local database using patient repository
+  Future<void> setLocalPatient(PatientEntity patient) async {
+    return await _repository.setLocalPatient(patient: patient);
+  }
+
+  // Set patient to remote database using patient repository
+  Future<void> setRemotePatient(PatientEntity patient) async {
+    return await _repository.setRemotePatient(patient: patient);
+  }
+
+  // Find patient from local database using patient repository
+  Future<PatientEntity?> findPatient(String id) async {
+    return await _repository.findPatient(id: id);
+  }
+}
