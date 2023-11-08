05f7a50956a3303b133d0ea3f8461af9cfb8c4d2
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-09 04:47:33 +0800

    feat: Add local and remote data sources and repository implementation for user feature

---
+++ lib/features/app/domain/repositories/user_repository.dart
@@ -0,0 +1,12 @@
+import 'package:health_worker/features/app/domain/domain.dart';
+
+abstract class UserRepository {
+  // Get user from remote database
+  Future<UserEntity> getRemoteUser();
+
+  // Get user from local database
+  Future<UserEntity?> getUser();
+
+  // Set user to local database
+  Future<void> setUser({required UserEntity user});
+}
