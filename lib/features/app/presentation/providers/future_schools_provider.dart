d04752bf224f2acc872effd6dfaecc6ac828d751
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-10 03:05:40 +0800

    feat: futureSchoolsProvider now filter schools by assignments.

--- lib/features/app/presentation/providers/future_schools_provider.dart
+++ lib/features/app/presentation/providers/future_schools_provider.dart
@@ -4,16 +4,29 @@ import 'package:health_worker/core/core.dart';
 import 'package:health_worker/features/features.dart';
 
 final futureSchoolsProvider = FutureProvider<List<SchoolEntity>>((ref) async {
-  final schools = await useCases.schoolsUseCase.getRemoteSchools();
-  await useCases.schoolsUseCase.setSchools(schools);
-  ref.read(schoolsProvider.notifier).setSchool(schools);
-  return schools;
+  final response = await useCases.schoolsUseCase.getRemoteSchools();
+  
+  if (response.isNotEmpty) {
+    final List<AssignmentEntity> assignments = ref.watch(assignmentProvider);
+
+    if (assignments.isNotEmpty) {
+      final school = response.where((element) => assignments.any((assignment) => assignment.school == element.id)).toList();
+
+      await useCases.schoolsUseCase.setSchools(school);
+      
+      ref.read(schoolsProvider.notifier).setSchool(school);
+
+      return response;
+    }
+  }
+
+  return response;
 });
 
 class SchoolsNotifier extends StateNotifier<List<SchoolEntity>> {
-  SchoolsNotifier(): super([]);
+  SchoolsNotifier() : super([]);
 
-  setSchool(List<SchoolEntity> schools) => state = schools; 
+  setSchool(List<SchoolEntity> schools) => state = schools;
 
   SchoolEntity findSchool(String id) {
     final school = state.where((element) => element.id == id).first;
