eb8462c0c195eb6d84f3a847f99295a5ac5c90e8
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-10 15:43:27 +0800

    feat: Add auto-suggest functionality to search box

--- lib/features/app/presentation/widgets/dashboard/search_box.dart
+++ lib/features/app/presentation/widgets/dashboard/search_box.dart
@@ -2,21 +2,32 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:health_worker/core/core.dart';
+import 'package:health_worker/features/app/app.dart';
 
 class SearchBox extends ConsumerWidget {
   const SearchBox({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    
-    return const SizedBox(
+    final List<TableEntity> table = ref.watch(tableRecordsProvider);
+    List<AutoSuggestBoxItem> items = table
+        .map<AutoSuggestBoxItem<TableEntity>>(
+          (record) => AutoSuggestBoxItem<TableEntity>(
+            label: record.name,
+            value: record,
+            child: Text(record.name),
+          ),
+        )
+        .toList();
+
+    return SizedBox(
       width: 450,
       child: AutoSuggestBox(
         placeholder: searchPatientText,
-        items: [],
+        items: items,
         // TODO: [OT-32] Implement search
-        // onSelected: (value) => ref.read(tabProvider.notifier).newTab(value.value!),
-        leadingIcon: Padding(
+        onSelected: (value) => print(value.label),
+        leadingIcon: const Padding(
           padding: EdgeInsets.only(left: 8.0),
           child: Icon(FluentIcons.search),
         ),
