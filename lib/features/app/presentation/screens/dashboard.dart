1f7c05f771cedc9244fb9cfe901071a856d3c47e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-10 15:42:49 +0800

    feat: Initialize Encryption Repository and update FutureBuilder in Dashboard screen

--- lib/features/app/presentation/screens/dashboard.dart
+++ lib/features/app/presentation/screens/dashboard.dart
@@ -2,7 +2,6 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:health_worker/core/core.dart';
-import 'package:health_worker/features/app/presentation/providers/future_table_provider.dart';
 import 'package:health_worker/features/features.dart';
 
 class Dashboard extends ConsumerWidget {
@@ -10,10 +9,12 @@ class Dashboard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Initialize Encryption Repository
+    // encryptionRepository = createEncryptionRepository("UjU4OUZ0b3E0Mm1uTStZRS82NkIyZW83bzhiN2NOZFFwNm9EcDNNaWN6UjFOOW5sS3RsZWhQOW10WlVYa1ZRcQ==");
+
     return ApplicationContainer(
       child: FutureBuilder(
-        future: Future.wait(
-          [
+        future: Future.wait([
           ref.watch(futureUserProvider.future),
           ref.watch(futurePatientsProvider.future),
           ref.watch(futureDoctorsProvider.future),
