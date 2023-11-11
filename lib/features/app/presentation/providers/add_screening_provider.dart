77d7c0b6e50430f4dc5ba85e095f6e37aee55289
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-11-12 01:15:23 +0800

    chore: Add clean state function to clean all screening state

--- lib/features/app/presentation/providers/add_screening_provider.dart
+++ lib/features/app/presentation/providers/add_screening_provider.dart
@@ -4,19 +4,34 @@ import 'package:health_worker/core/core.dart';
 import 'package:health_worker/features/app/app.dart';
 
 class ScreeningNotifier extends StateNotifier<ScreeningEntity> {
-  ScreeningNotifier() : super(emptyScreening);
+  final StateNotifierProviderRef<ScreeningNotifier, ScreeningEntity> ref;
+  
+  ScreeningNotifier(this.ref) : super(emptyScreening);
 
   setScreening(ScreeningEntity screening) {
     useCases.screeningsUseCases.setLocalScreening(screening);
-    useCases.screeningsUseCases.setRemoteScreening(screening);
-
     state = screening;
+  }
+
+  cleanStates() {
+    ref.read(historyOfIllnessProvider.notifier).setState("");
+    ref.read(healthWorkerCommentProvider.notifier).setState("");
+    ref.read(temperaturenessProvider.notifier).setState("");
+    ref.read(heightProvider.notifier).setState("");
+    ref.read(weightProvider.notifier).setState("");
+    ref.read(similarConditionProvider.notifier).setState(3);
+    ref.read(cheifComplainProvider.notifier).setStatetoEmpty();
+    ref.read(otherComplainProvider.notifier).setState("");
+    ref.read(haveAllergiesProvider.notifier).setState(3);
+    ref.read(undergoSurgeryProvider.notifier).setState(3);
+    ref.read(takingMedicationProvider.notifier).setState(3);
+    ref.read(medicationProvider.notifier).setState("");
   }
 }
 
 final screeningProvider =
     StateNotifierProvider<ScreeningNotifier, ScreeningEntity>((ref) {
-  return ScreeningNotifier();
+  return ScreeningNotifier(ref);
 });
 
 ScreeningEntity emptyScreening = ScreeningEntity(
