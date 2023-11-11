85ac39f466cd77805815d95af20e87d9f77d5443
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-11-12 01:17:44 +0800

    Refactor screening information form

--- lib/features/app/presentation/screens/screening.dart
+++ lib/features/app/presentation/screens/screening.dart
@@ -31,8 +31,6 @@ class ScreeningInformation extends ConsumerWidget {
                       const Divider(),
                       largeHeight,
                       const ProgressButtons(status: 4),
-                      largeWidth,
-                      const ExaminerAndDate(),
                       largeHeight,
                       const IllnessAndComment(),
                       largeHeight,
@@ -70,26 +68,18 @@ class ScreeningInformation extends ConsumerWidget {
   }
 }
 
-addScreeningInformation(
-    BuildContext context, WidgetRef ref, GlobalKey<FormState> key) async {
+addScreeningInformation(BuildContext context, WidgetRef ref, GlobalKey<FormState> key) async {
   String historyOfIllness = ref.read(historyOfIllnessProvider);
   String healthWorkerComment = ref.read(healthWorkerCommentProvider);
   String temperature = ref.read(temperaturenessProvider);
   String height = ref.read(heightProvider);
   String weight = ref.read(weightProvider);
-  String similarCondition =
-      ref.read(similarConditionProvider) == 1 ? "true" : "false";
+  String similarCondition = ref.read(similarConditionProvider) == 1 ? "true" : "false";
   List<bool> cheifComplains = ref.read(cheifComplainProvider);
-  String chiefComplainStrings = List.generate(
-          cheifComplains.length, (index) => checker(cheifComplains, index))
-      .toString();
-  List<String> arrayComplain = chiefComplainStrings
-      .substring(1, chiefComplainStrings.length - 1)
-      .split(", ");
-  Iterable<String> nonEmptyComplains =
-      arrayComplain.where((complain) => complain.isNotEmpty);
-  String joinedComplains =
-      nonEmptyComplains.toList().asMap().entries.map((entry) {
+  String chiefComplainStrings = List.generate(cheifComplains.length, (index) => checker(cheifComplains, index)).toString();
+  List<String> arrayComplain = chiefComplainStrings.substring(1, chiefComplainStrings.length - 1).split(", ");
+  Iterable<String> nonEmptyComplains = arrayComplain.where((complain) => complain.isNotEmpty);
+  String joinedComplains = nonEmptyComplains.toList().asMap().entries.map((entry) {
     final index = entry.key;
     final complain = entry.value;
     if (index == nonEmptyComplains.length - 1) {
@@ -102,26 +92,16 @@ addScreeningInformation(
   }).join(' ');
   String otherComplains = ref.read(otherComplainProvider);
   String allergies = ref.read(haveAllergiesProvider) == 1 ? "true" : "false";
-  String surgicalProcedure =
-      ref.read(undergoSurgeryProvider) == 1 ? "true" : "false";
-  String medication =
-      ref.read(takingMedicationProvider) == 1 ? "true" : "false";
+  String surgicalProcedure = ref.read(undergoSurgeryProvider) == 1 ? "true" : "false";
+  String medication = ref.read(takingMedicationProvider) == 1 ? "true" : "false";
   String medicationComment = ref.read(medicationProvider);
 
   final List<AssignmentEntity> assignments = ref.read(assignmentProvider);
-  final String user = ref.read(userProvider).id;
   final String school = ref.read(patientProvider).school;
 
-  assignments.removeWhere((element) => element.nurse != user);
   assignments.removeWhere((element) => element.school != school);
 
-  if (key.currentState!.validate() &&
-          similarCondition.isNotEmpty &&
-          cheifComplains.contains(true) ||
-      (cheifComplains.contains(true) && otherComplains.isEmpty) &&
-          allergies.isNotEmpty &&
-          surgicalProcedure.isNotEmpty &&
-          (medication.isNotEmpty && medicationComment.isNotEmpty)) {
+  if (key.currentState!.validate() && similarCondition.isNotEmpty && cheifComplains.contains(true) || (cheifComplains.contains(true) && otherComplains.isEmpty) && allergies.isNotEmpty && surgicalProcedure.isNotEmpty && (medication.isNotEmpty && medicationComment.isNotEmpty)) {
     ScreeningEntity screening = ScreeningEntity(
       id: uuid.v4(),
       patient: ref.read(patientProvider).id,
@@ -139,16 +119,17 @@ addScreeningInformation(
       typeOfAllergies: "TO BE CONTINUED",
       undergoSurgery: surgicalProcedure,
       takingMedication: medication,
-      takingMedicationMessage:
-          medicationComment.isEmpty ? "" : medicationComment,
+      takingMedicationMessage: medicationComment.isEmpty ? "" : medicationComment,
       status: "Pending",
-      createdAt: "", images: [],
+      createdAt: "",
+      // TODO: [OT-41] Link patients medical record to screening
+      images: [],
     );
 
     ref.read(screeningProvider.notifier).setScreening(screening);
 
-    Navigator.push(context,
-        FluentPageRoute(builder: ((context) => const ReviewInformation())));
+    Navigator.push(context, FluentPageRoute(builder: ((context) => const ReviewInformation())));
+    ref.read(screeningProvider.notifier).cleanStates();
   } else {
     if (key.currentState!.validate()) {}
     if (similarCondition.isEmpty) {
