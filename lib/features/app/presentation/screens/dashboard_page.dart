d2087404944ad3ed82a06c6234a6ff7b7e1c69d0
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-16 05:46:06 +0800

    chore: Remove duplicate import

--- lib/features/app/presentation/screens/dashboard_page.dart
+++ lib/features/app/presentation/screens/dashboard_page.dart
@@ -2,7 +2,6 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:health_worker/core/core.dart';
-import 'package:health_worker/features/app/presentation/providers/future_provider.dart';
 import 'package:health_worker/features/features.dart';
 
 class Dashboard extends ConsumerWidget {
