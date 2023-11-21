dfc424957f21135f27f9ed5f6f4c62b2c6381a1c
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-11-22 03:00:18 +0800

    feat: Add online and offline sync when application starts

--- lib/features/app/presentation/providers/future_providers/future_screening_provider.dart
+++ lib/features/app/presentation/providers/future_providers/future_screening_provider.dart
@@ -3,8 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:health_worker/core/core.dart';
 import 'package:health_worker/features/features.dart';
 
+/// Fetch and handle screenings based on the user's online status.
 Future<void> screenings(
     WidgetRef ref, List<AssignmentEntity> assignments) async {
+  // Check if the user is online
+  final bool isOnline = ref.read(networkProvider);
+
   // Late declare screenings variable
   late final List<ScreeningEntity> screenings;
 
@@ -18,33 +22,60 @@ Future<void> screenings(
   local = await useCases.screeningsUseCases.getLocalScreenings();
 
   // Check if user is online
-  if (ref.read(networkProvider)) {
-    // Check if assignments is not empty
-    if (assignments.isNotEmpty) {
-      // Get remote screenings
-      cloud = await useCases.screeningsUseCases.getRemoteScreenings();
+  if (isOnline) {
+    // Get remote screenings
+    cloud = await useCases.screeningsUseCases.getRemoteScreenings();
 
-      // Check if screenings are in assignments list and initialize screenings
-      screenings = cloud
-          .where((screening) => assignments
-              .any((assignment) => assignment.id == screening.assignment))
-          .toList();
+    // Filter screenings based on assignments
+    screenings = cloud
+        .where((screening) => assignments
+            .any((assignment) => assignment.id == screening.assignment))
+        .toList();
 
-      // Filter screenings that is not in local storage
-      List<ScreeningEntity> filter = screenings
-          .where((screening) =>
-              !local.any((element) => element == screening))
-          .toList();
+    // Check if local is not empty
+    if (local.isNotEmpty) {
+      // Check if local screenings need to be updated
+      if (local.length > screenings.length) {
+        // Filter local screenings not present in the cloud
+        List<ScreeningEntity> filter = local
+            .where((screening) => !screenings
+                .any((element) => element.assignment == screening.assignment))
+            .toList();
 
-      // Set screenings to local storage
-      await useCases.screeningsUseCases.setScreenings(filter);
+        // upload filtered screenings to cloud
+        filter.map((screening) async {
+          await useCases.screeningsUseCases.setRemoteScreening(screening);
+        });
+
+        List<ScreeningEntity> updatedScreenings = screenings + filter;
+
+        // Update local screenings with cloud screenings
+        await _updateLocalScreenings(updatedScreenings, ref);
+      } else {
+        // Update local screenings with cloud screenings
+        await _updateLocalScreenings(screenings, ref);
+        return;
+      }
+    } else {
+      // No local data, update local screenings with cloud screenings
+      _updateLocalScreenings(screenings, ref);
+      return;
     }
-    await useCases.screeningsUseCases.setScreenings(screenings);
   } else {
-    screenings = await useCases.screeningsUseCases.getLocalScreenings();
+    // User is offline, use local data
+    ref.read(screeningsProvider.notifier).setScreenings(local);
+    return;
   }
+}
 
-  ref.read(screeningsProvider.notifier).setScreenings(screenings);
+/// Update local screenings and notify the provider.
+Future<void> _updateLocalScreenings(
+    List<ScreeningEntity> updatedScreenings, WidgetRef ref) async {
+  // Update local screenings in storage
+  await useCases.screeningsUseCases.setScreenings(updatedScreenings);
+
+  // Notify the provider with the updated screenings
+  ref.read(screeningsProvider.notifier).setScreenings(updatedScreenings);
 }
 
 class ScreeningsNotifier extends StateNotifier<List<ScreeningEntity>> {
