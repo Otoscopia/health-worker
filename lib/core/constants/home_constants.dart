8c86b8624f4744500d18fb9097305aed9cdc79e3
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-12-02 18:42:47 +0800

    chore: Add home page title and tabs

--- lib/core/constants/home_constants.dart
+++ lib/core/constants/home_constants.dart
@@ -1,3 +1,7 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+import 'package:health_worker/features/features.dart';
+
 const String tableColumnName = "Name";
 const String tableColumnAge = "Age";
 const String tableColumnGender = "Gender";
@@ -7,3 +11,15 @@ const String tableColumnSchool = "School";
 
 const String noRecord = "No Record found";
 const String homeTooltip = 'Add New Patient';
+
+const String homePageTitle = 'Home';
+
+List<Tab> tabs = [
+  Tab(
+    text: const Text(homePageTitle),
+    body: const HomeTable(),
+    closeIcon: null,
+    icon: const Icon(FluentIcons.home),
+    semanticLabel: homePageTitle,
+  ),
+];
