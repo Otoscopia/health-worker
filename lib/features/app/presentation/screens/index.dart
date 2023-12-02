ce064324a04d7628bd0aef198325b3de896faa8e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-12-02 19:16:21 +0800

    feat: Create index.dart that will export all screens

---
+++ lib/features/app/presentation/screens/index.dart
@@ -0,0 +1,24 @@
+// Dashboard
+export 'dashboard/home_page.dart';
+
+// Patient Information
+export 'patient_information/patient_information.dart';
+export 'patient_information/patient_record.dart';
+
+// Screening Process
+export 'screening_process/add_patient.dart';
+export 'screening_process/left_camera.dart';
+export 'screening_process/review_information.dart';
+export 'screening_process/right_camera.dart';
+export 'screening_process/screening.dart';
+
+// Settings
+export 'settings/account_information.dart';
+export 'settings/settings_page.dart';
+
+// Individual Pages
+export 'doctors/doctors_page.dart';
+export 'error_page.dart';
+export 'loading_page.dart';
+export 'patients/patients_page.dart';
+export 'schools/schools_page.dart';
