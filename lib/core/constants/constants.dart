d9e8b0de6e0a5191263ed9c228b81bd1ebf4707e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-12-02 18:51:32 +0800

    chore: Icons now have color of primary

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,10 +1,12 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:ionicons/ionicons.dart';
 
+import 'package:health_worker/config/config.dart';
+
 const String logoSvg = 'assets/logo.svg';
 const String otoscopia = 'assets/otoscopia-text.svg';
 
-const String applicationTitle = "Otoscopia ENT Website";
+const String applicationTitle = "Otoscopia Application";
 
 const String offlineText = "You are offline";
 
@@ -15,4 +17,17 @@ const String continueLabel = "Continue";
 
 const Icon closeButtonIcon = Icon(Ionicons.close_outline);
 
-List<Icon> icons = [const Icon(Ionicons.barbell), const Icon(Ionicons.body), const Icon(Ionicons.thermometer)];
+List<Icon> icons = [
+  const Icon(
+    Ionicons.barbell,
+    color: primary,
+  ),
+  const Icon(
+    Ionicons.body,
+    color: primary,
+  ),
+  const Icon(
+    Ionicons.thermometer,
+    color: primary,
+  )
+];
