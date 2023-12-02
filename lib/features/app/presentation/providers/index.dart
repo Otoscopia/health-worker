1cbee09476cbd4cb4f38cbbd09f631ab81cf8156
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-12-02 18:41:15 +0800

    feat: Create index that will export all providers

---
+++ lib/features/app/presentation/providers/index.dart
@@ -0,0 +1,29 @@
+export 'add_patient_provider.dart';
+export 'add_screening_provider.dart';
+export 'application_provider.dart';
+export 'future_provider.dart';
+export 'tab_provider.dart';
+
+export 'future_providers/future_assignment_provider.dart';
+export 'future_providers/future_doctor_provider.dart';
+export 'future_providers/future_patient_provider.dart';
+export 'future_providers/future_schools_provider.dart';
+export 'future_providers/future_screening_provider.dart';
+export 'future_providers/future_table_provider.dart';
+export 'future_providers/future_user_provider.dart';
+
+export 'add_patient_providers/birthdate_provider.dart';
+export 'add_patient_providers/contact_number_provider.dart';
+export 'add_patient_providers/fullname_provider.dart';
+export 'add_patient_providers/gender_provider.dart';
+export 'add_patient_providers/guardian_fullname_provider.dart';
+export 'add_patient_providers/school_id.dart';
+export 'add_patient_providers/school_name_provider.dart';
+
+export 'screening/cheif_complain.dart';
+export 'screening/complain_provider.dart';
+export 'screening/height_weight_provider.dart';
+export 'screening/history_and_comment.dart';
+export 'screening/medication_provider.dart';
+export 'screening/similar_condition_and_allergies.dart';
+export 'screening/temperature_and_blood_provider.dart';
