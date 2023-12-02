697017c99f7e8cd94000d9e4c1e158db6beae660
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-12-02 19:16:28 +0800

    feat: Create index.dart that will export all widgets

---
+++ lib/features/app/presentation/widgets/index.dart
@@ -0,0 +1,59 @@
+// Add patient widgets
+export 'add_patient/add_patient_button.dart';
+export 'add_patient/birthdate_widget.dart';
+export 'add_patient/birthday_error.dart';
+export 'add_patient/gender_error.dart';
+export 'add_patient/gender_widget.dart';
+export 'add_patient/text_inputs_widgets.dart/full_name_input.dart';
+export 'add_patient/text_inputs_widgets.dart/guardians_name.dart';
+export 'add_patient/text_inputs_widgets.dart/guarians_phone_number.dart';
+export 'add_patient/text_inputs_widgets.dart/school_id_input.dart';
+export 'add_patient/text_inputs_widgets.dart/school_name_input.dart';
+
+// Application widgets
+export 'application/application_header.dart';
+export 'application/search_box.dart';
+export 'application/user_container.dart';
+
+// Home widgets
+export 'home/floating_action_button.dart';
+export 'home/home_table.dart';
+export 'home/home_table_source.dart';
+
+// Patient records widgets
+export 'patient_records/information_container.dart';
+export 'patient_records/information_card.dart';
+export 'patient_records/medical_record_card.dart';
+export 'patient_records/patient_information.dart';
+export 'patient_records/medical_record/doctors_note.dart';
+export 'patient_records/medical_record/nurse_note.dart';
+
+// Review information widgets
+export 'review_information/health_record_title_widget.dart';
+export 'review_information/patient_information.dart';
+export 'review_information/vital_card.dart';
+export 'review_information/vital_widget.dart';
+
+// Screening widgets
+export 'screening/cheif_complain_error.dart';
+export 'screening/cheif_complain_widget.dart';
+export 'screening/cheif_complain.dart';
+export 'screening/examiner_and_date.dart';
+export 'screening/height_and_weight.dart';
+export 'screening/illness_and_comment.dart';
+export 'screening/medication_widget.dart';
+export 'screening/other_complain.dart';
+export 'screening/patient_allergies_widget.dart';
+export 'screening/patient_have_allergies.dart';
+export 'screening/patient_medication_widget.dart';
+export 'screening/patient_similar_condition.dart';
+export 'screening/patient_taking_medication.dart';
+export 'screening/patient_undergo_surgery.dart';
+export 'screening/patient_undergo_surgery_widget.dart';
+export 'screening/similar_condition_error.dart';
+export 'screening/similar_condition_widget.dart';
+export 'screening/temperature.dart';
+
+// Individual widgets
+export 'progress_buttons.dart';
+export 'title_widget.dart';
